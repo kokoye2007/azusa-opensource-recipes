8aa54947fb98d0b31683551c5a19c6ae0ebeaa85
Mark Karpeles <magicaltux@gmail.com>
2019-04-20 15:54:16 +0900

    fixed glibc

---
+++ sys-libs/glibc/glibc-2.29.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+# fetch xz, compile, build
+get http://ftp.jaist.ac.jp/pub/GNU/libc/${P}.tar.xz
+
+if [ ! -d ${P} ]; then
+	echo "Extracting ${P} ..."
+	tar xf ${P}.tar.xz
+fi
+
+echo "Compiling ${P} ..."
+cd "${T}"
+
+# configure & build
+${CHPATH}/${P}/configure >configure.log 2>&1 --prefix=/pkg/main/${PKG}.core.${PVR}  --sysconfdir=/etc \
+--includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR}/lib --datarootdir=/pkg/main/${PKG}.core.${PVR}/share \
+--mandir=/pkg/main/${PKG}.doc.${PVR}/man --docdir=/pkg/main/${PKG}.doc.${PVR}/doc
+
+make >make.log 2>&1
+make >make_install.log 2>&1 install DESTDIR="${D}"
+
+cd "${D}"
+
+# move some stuff
+mv etc "pkg/main/${PKG}.core.${PVR}"
+#mv sbin/* "pkg/main/${PKG}.core.${PVR}/sbin"
+#mv lib* "pkg/main/${PKG}.libs.${PVR}"
+#mv var "pkg/main/${PKG}.core.${PVR}"
+#rmdir sbin
+
+finalize
+cleanup
