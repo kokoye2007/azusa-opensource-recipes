625e2c98a56ba1ed27de6ab951c1682bebacb70c
Mark Karpeles <magicaltux@gmail.com>
2019-04-21 01:02:40 +0900

    add attr, fix acl

--- sys-apps/attr/attr-2.4.48.sh
+++ sys-apps/attr/attr-2.4.48.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source "../../common/init.sh"
 
-get http://download.savannah.nongnu.org/releases/acl/${P}.tar.gz
+get http://download.savannah.nongnu.org/releases/attr/${P}.tar.gz
 
 if [ ! -d ${P} ]; then
 	echo "Extracting ${P} ..."
@@ -12,12 +12,15 @@ echo "Compiling ${P} ..."
 cd "${T}"
 
 # configure & build
-${CHPATH}/${P}/configure >configure.log 2>&1 --prefix=/usr --sysconfdir=/etc --exec-prefix=/pkg/main/${PKG}.core.${PVR} \
+${CHPATH}/${P}/configure >configure.log 2>&1 --prefix=/pkg/main/${PKG}.core.${PVR} --sysconfdir=/etc \
 --includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR}/lib --datarootdir=/pkg/main/${PKG}.core.${PVR} \
 --mandir=/pkg/main/${PKG}.doc.${PVR}/man --docdir=/pkg/main/${PKG}.doc.${PVR}/doc
 
 make >make.log 2>&1
 make >make_install.log 2>&1 install DESTDIR="${D}"
+
+cd "${D}"
+mv "etc" "pkg/main/${PKG}.core.${PVR}"
 
 finalize
 cleanup
