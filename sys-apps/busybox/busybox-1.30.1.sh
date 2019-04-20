0059d6d4a6a3495a010ec0493da9da2ad4dd07d3
Mark Karpeles <magicaltux@gmail.com>
2019-04-20 14:34:02 +0900

    updated busybox

---
+++ sys-apps/busybox/busybox-1.30.1.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://busybox.net/downloads/${P}.tar.bz2
+
+if [ ! -d ${P} ]; then
+	echo "Extracting ${P} ..."
+	tar xf ${P}.tar.bz2
+fi
+
+echo "Compiling ${P} ..."
+cd "${P}"
+cp $FILESDIR/config-${PV} .config
+
+make >make.log 2>&1
+make >make_install.log 2>&1 install
+
+mkdir -p "${D}/pkg/main/${PKG}.${PVR}/"
+rsync -a ./_install/ "${D}/pkg/main/${PKG}.${PVR}/"
+
+finalize
