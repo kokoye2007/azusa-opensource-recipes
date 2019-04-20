3a4b95cab5bac2f81d83a8cd8da23b2c4bcde63b
Mark Karpeles <magicaltux@gmail.com>
2019-04-21 00:52:30 +0900

    fix file compile

--- sys-apps/file/file-5.36.sh
+++ sys-apps/file/file-5.36.sh
@@ -12,9 +12,9 @@ echo "Compiling ${P} ..."
 cd "${T}"
 
 # configure & build
-${CHPATH}/${P}/configure >configure.log 2>&1 --prefix=/pkg/main/${PKG}.core.${FILE_VER} --sysconfdir=/etc \
---includedir=/pkg/main/${PKG}.dev.${FILE_VER}/include --libdir=/pkg/main/${PKG}.libs.${FILE_VER} --datarootdir=/pkg/main/${PKG}.core.${FILE_VER}/share \
---mandir=/pkg/main/${PKG}.doc.${FILE_VER}/man --docdir=/pkg/main/${PKG}.doc.${FILE_VER}/doc
+${CHPATH}/${P}/configure >configure.log 2>&1 --prefix=/pkg/main/${PKG}.core.${PVR} --sysconfdir=/etc \
+--includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR} --datarootdir=/pkg/main/${PKG}.core.${PVR}/share \
+--mandir=/pkg/main/${PKG}.doc.${PVR}/man --docdir=/pkg/main/${PKG}.doc.${PVR}/doc
 
 make >make.log 2>&1
 make >make_install.log 2>&1 install DESTDIR="${D}"
