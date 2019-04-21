4fb93c5f028ad7f97f91ed2a18d35d7dbdad633f
Mark Karpeles <magicaltux@gmail.com>
2019-04-21 13:25:32 +0900

    pam fix

--- sys-libs/pam/pam-1.3.1.sh
+++ sys-libs/pam/pam-1.3.1.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 source "../../common/init.sh"
 
-get https://ftp.gnu.org/gnu/m4/${P}.tar.xz
+get https://github.com/linux-pam/linux-pam/releases/download/v${PV}/Linux-PAM-${PV}.tar.xz
 
-patch -p0 <"$FILESDIR/m4-1.4.18-glibc-change-work-around.patch"
-
-echo "Compiling ${P} ..."
+echo "Compiling Linux-PAM-${PV} ..."
 cd "${T}"
 
 # configure & build
-${CHPATH}/${P}/configure >configure.log 2>&1 --prefix=/pkg/main/${PKG}.core.${PVR} --sysconfdir=/etc \
+${CHPATH}/Linux-PAM-${PV}/configure >configure.log 2>&1 --prefix=/usr --sysconfdir=/etc --exec-prefix=/pkg/main/${PKG}.core.${PVR} \
 --includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR} --datarootdir=/pkg/main/${PKG}.core.${PVR}/share \
 --mandir=/pkg/main/${PKG}.doc.${PVR}/man --docdir=/pkg/main/${PKG}.doc.${PVR}/doc
 
 make >make.log 2>&1
 make >make_install.log 2>&1 install DESTDIR="${D}"
 
+cd "${D}"
+
+mv etc pkg/main/${PKG}.core.${PVR}
+mv sbin pkg/main/${PKG}.core.${PVR}
+
 finalize
-cleanup
