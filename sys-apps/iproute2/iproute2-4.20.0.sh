812c910178a94ea3241ed4b1f0f513c0ca4165f7
Mark Karpeles <magicaltux@gmail.com>
2019-04-21 21:40:54 +0900

    add iproute2

--- sys-apps/iproute2/iproute2-4.20.0.sh
+++ sys-apps/iproute2/iproute2-4.20.0.sh
@@ -10,4 +10,13 @@ doconf
 make
 make install DESTDIR="${D}"
 
+cd "${D}"
+
+mkdir -p "pkg/main/${PKG}.core.${PVR}" "pkg/main/${PKG}.libs.${PVR}" "pkg/main/${PKG}.doc.${PVR}" "pkg/main/${PKG}.dev.${PVR}"
+mv etc var sbin "pkg/main/${PKG}.core.${PVR}"
+mv usr/lib "pkg/main/${PKG}.libs.${PVR}"
+mv usr/share/{doc,man} "pkg/main/${PKG}.doc.${PVR}"
+mv usr/include "pkg/main/${PKG}.dev.${PVR}"
+rm -fr usr
+
 finalize
