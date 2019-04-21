e7596e277becc7e6f1067f22e84bad2487c946d7
Mark Karpeles <magicaltux@gmail.com>
2019-04-21 10:12:23 +0900

    bison fix

---
+++ sys-devel/bison/bison-3.3.2.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get http://ftp.gnu.org/gnu/bison/${P}.tar.xz
+
+if [ ! -d ${P} ]; then
+	echo "Extracting ${P} ..."
+	tar xf ${P}.tar.xz
+fi
+
+echo "Compiling ${P} ..."
+cd "${T}"
+
+# configure & build
+${CHPATH}/${P}/configure >configure.log 2>&1 --prefix=/pkg/main/${PKG}.core.${PVR} --sysconfdir=/etc \
+--includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR}/lib --datarootdir=/pkg/main/${PKG}.core.${PVR} \
+--mandir=/pkg/main/${PKG}.doc.${PVR}/man --docdir=/pkg/main/${PKG}.doc.${PVR}/doc
+
+make >make.log 2>&1
+make >make_install.log 2>&1 install DESTDIR="${D}"
+
+finalize
+cleanup
