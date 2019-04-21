191bf5cd63b73af009830455e2e5b51c7c3824ab
Mark Karpeles <magicaltux@gmail.com>
2019-04-21 12:45:57 +0900

    fix v

--- dev-libs/mpfr/mpfr-4.0.2.sh
+++ dev-libs/mpfr/mpfr-4.0.2.sh
@@ -12,9 +12,9 @@ echo "Compiling ${P} ..."
 cd "${T}"
 
 # configure & build
-${CHPATH}/${P}/configure >configure.log 2>&1 --prefix=/pkg/main/${PKG}.core.${MPFR_VER} --sysconfdir=/etc \
---includedir=/pkg/main/${PKG}.dev.${MPFR_VER}/include --libdir=/pkg/main/${PKG}.libs.${MPFR_VER}/lib --datarootdir=/pkg/main/${PKG}.core.${MPFR_VER}/share \
---mandir=/pkg/main/${PKG}.doc.${MPFR_VER}/man --docdir=/pkg/main/${PKG}.doc.${MPFR_VER}/doc \
+${CHPATH}/${P}/configure >configure.log 2>&1 --prefix=/pkg/main/${PKG}.core.${PVR} --sysconfdir=/etc \
+--includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR}/lib --datarootdir=/pkg/main/${PKG}.core.${PVR}/share \
+--mandir=/pkg/main/${PKG}.doc.${PVR}/man --docdir=/pkg/main/${PKG}.doc.${PVR}/doc \
 --disable-static --enable-thread-safe
 
 make >make.log 2>&1
