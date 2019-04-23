1dd430c66db94caf376719efc21b202074e52646
Mark Karpeles <magicaltux@gmail.com>
2019-04-23 21:33:07 +0900

    more fix

--- sys-devel/gettext/gettext-0.19.8.1.sh
+++ sys-devel/gettext/gettext-0.19.8.1.sh
@@ -16,7 +16,7 @@ cd "${T}"
 
 doconf --disable-static
 
-make >make.log 2>&1
-make >make_install.log 2>&1 install DESTDIR="${D}"
+make
+make install DESTDIR="${D}"
 
 finalize
--- sys-devel/m4/m4-1.4.18.sh
+++ sys-devel/m4/m4-1.4.18.sh
@@ -9,12 +9,9 @@ echo "Compiling ${P} ..."
 cd "${T}"
 
 # configure & build
-${CHPATH}/${P}/configure >configure.log 2>&1 --prefix=/pkg/main/${PKG}.core.${PVR} --sysconfdir=/etc \
---includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR} --datarootdir=/pkg/main/${PKG}.core.${PVR}/share \
---mandir=/pkg/main/${PKG}.doc.${PVR}/man --docdir=/pkg/main/${PKG}.doc.${PVR}/doc
+doconf
 
-make >make.log 2>&1
-make >make_install.log 2>&1 install DESTDIR="${D}"
+make
+make install DESTDIR="${D}"
 
 finalize
-cleanup
