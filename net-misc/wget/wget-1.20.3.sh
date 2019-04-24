0c82d6effc9dedfc25a7b3e6dd32b2cce9fc79fe
Mark Karpeles <magicaltux@gmail.com>
2019-04-25 02:54:44 +0900

    adding libs & stuff

---
+++ net-misc/wget/wget-1.20.3.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get http://ftp.gnu.org/gnu/${PN}/${P}.tar.gz
+
+cd "${T}"
+
+# configure & build
+doconf --with-ssl=openssl --with-libssl-prefix=`realpath /pkg/main/dev-libs.openssl.dev/`
+
+make
+make install DESTDIR="${D}"
+
+finalize
