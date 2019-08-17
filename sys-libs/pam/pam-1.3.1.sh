9454517864bcd57870f3b6fe40159cbb1427325a
Mark Karpeles <magicaltux@gmail.com>
2019-08-17 09:43:11 +0900

    update

--- sys-libs/pam/pam-1.3.1.sh
+++ sys-libs/pam/pam-1.3.1.sh
@@ -7,12 +7,12 @@ echo "Compiling Linux-PAM-${PV} ..."
 cd "${T}"
 
 # configure & build
-${CHPATH}/Linux-PAM-${PV}/configure >configure.log 2>&1 --prefix=/usr --sysconfdir=/etc --exec-prefix=/pkg/main/${PKG}.core.${PVR} \
+${CHPATH}/Linux-PAM-${PV}/configure --prefix=/usr --sysconfdir=/etc --exec-prefix=/pkg/main/${PKG}.core.${PVR} \
 --includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR} --datarootdir=/pkg/main/${PKG}.core.${PVR}/share \
 --mandir=/pkg/main/${PKG}.doc.${PVR}/man --docdir=/pkg/main/${PKG}.doc.${PVR}/doc
 
-make >make.log 2>&1
-make >make_install.log 2>&1 install DESTDIR="${D}"
+make
+make install DESTDIR="${D}"
 
 cd "${D}"
 
