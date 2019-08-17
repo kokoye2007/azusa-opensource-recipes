852b954294b4c50a2870f2812c46cad2e4a5ad67
Mark Karpeles <magicaltux@gmail.com>
2019-08-17 14:26:17 +0900

    add nasm

---
+++ dev-lang/nasm/nasm-2.14.02.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://www.nasm.us/pub/nasm/releasebuilds/${PV}/${P}.tar.bz2
+
+cd "${T}"
+
+# configure & build
+doconf
+
+make
+make install DESTDIR="${D}"
+
+finalize
