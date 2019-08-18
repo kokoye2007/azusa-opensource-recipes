1c6338beda95ca24890d2037b3501d13e5bbab65
Mark Karpeles <magicaltux@gmail.com>
2019-08-18 10:07:07 +0900

    add lld (not working)

---
+++ sys-devel/lld/lld-8.0.1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/llvm/llvm-project/releases/download/llvmorg-${PV}/${P}.src.tar.xz
+get https://github.com/llvm/llvm-project/releases/download/llvmorg-${PV}/${P}.src.tar.xz
+
+cd "${T}"
+
+# see http://llvm.org/docs/CMake.html
+
+# make it possible to run llvm tools within a non-azusa linux (TEMP)
+export LD_LIBRARY_PATH=/pkg/main/sys-devel.llvm.dev.${PV}/lib64
+export LLVM_DIR=/pkg/main/sys-devel.llvm.dev.${PV}/lib64/cmake/llvm
+
+cmake ${CHPATH}/${P}.src -DLLVM_CONFIG=/pkg/main/sys-devel.llvm.dev.${PV}/bin/llvm-config -DCMAKE_INSTALL_PREFIX=/pkg/main/${PKG}.dev.${PVR} -DLLVM_ENABLE_TERMINFO=ON \
+	-DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_APPEND_VC_REV=OFF -DLLVM_INSTALL_UTILS=ON \
+	-DLLVM_LINK_LLVM_DYLIB=ON
+
+#cmake --build .
+make
+
+cmake -DCMAKE_INSTALL_PREFIX="${D}/pkg/main/${PKG}.dev.${PVR}" -P cmake_install.cmake
+
+cd "${D}"
+
+finalize
