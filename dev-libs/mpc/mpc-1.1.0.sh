5af7747dce001e47ea6bdd6b6f05053927cfdea9
Mark Karpeles <magicaltux@gmail.com>
2019-08-19 15:28:25 +0900

    add gcc & lha, fix mpc

--- dev-libs/mpc/mpc-1.1.0.sh
+++ dev-libs/mpc/mpc-1.1.0.sh
@@ -6,7 +6,7 @@ get https://ftp.gnu.org/gnu/mpc/${P}.tar.gz
 cd "${T}"
 
 # configure & build
-doconf --disable-static --with-mpfr-include=/pkg/main/dev-libs.mpfr.dev/include --with-mpfr-lib=`realpath /pkg/main/dev-libs.mpfr.libs/lib` \
+doconf --disable-static --with-mpfr-include=/pkg/main/dev-libs.mpfr.dev/include --with-mpfr-lib=`realpath /pkg/main/dev-libs.mpfr.libs/lib$LIB_SUFFIX` \
 --with-gmp-include=`realpath /pkg/main/dev-libs.gmp.dev/include` --with-gmp-lib=`realpath /pkg/main/dev-libs.gmp.libs/lib`
 
 make
