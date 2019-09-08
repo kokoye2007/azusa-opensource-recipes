0fcf744548e6e48144ff0c4be1bd21a412af1836
Mark Karpeles <magicaltux@gmail.com>
2019-09-09 00:31:51 +0900

    fix build for openssl 1.0.0

--- dev-libs/openssl/openssl-1.0.2s.sh
+++ dev-libs/openssl/openssl-1.0.2s.sh
@@ -5,7 +5,7 @@ get https://www.openssl.org/source/${P}.tar.gz
 
 cd "${P}"
 
-./config --prefix=/pkg/main/${PKG}.core.${PVR} --openssldir=/etc/ssl --libdir=/pkg/main/${PKG}.libs.${PVR}/lib shared zlib-dynamic
+./config --prefix=/pkg/main/${PKG}.core.${PVR} --openssldir=/etc/ssl shared zlib-dynamic
 
 make
 make install MANSUFFIX=ssl INSTALL_PREFIX="${D}"
