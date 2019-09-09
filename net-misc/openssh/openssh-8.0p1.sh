c31e6cd3d8fe99d3ada8bdbb1606f2e86b403969
Mark Karpeles <magicaltux@gmail.com>
2019-09-09 11:03:23 +0900

    openssh: using azusa openssl

--- net-misc/openssh/openssh-8.0p1.sh
+++ net-misc/openssh/openssh-8.0p1.sh
@@ -5,8 +5,10 @@ get https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/${P}.tar.gz
 
 cd "${T}"
 
+export LD_LIBRARY_PATH=$(realpath /pkg/main/dev-libs.openssl.dev/lib)
+
 # configure & build
-doconf
+doconf --with-zlib=`realpath /pkg/main/sys-libs.zlib.dev` --with-ssl-dir=`realpath /pkg/main/dev-libs.openssl.dev`
 
 make
 make install DESTDIR="${D}"
