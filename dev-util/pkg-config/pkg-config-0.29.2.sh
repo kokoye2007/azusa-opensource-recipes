c844aec314f2d403479b6e4bbc2196f0353a1273
Mark Karpeles <magicaltux@gmail.com>
2019-09-13 08:47:49 +0900

    fix pkgconfig search path

--- dev-util/pkg-config/pkg-config-0.29.2.sh
+++ dev-util/pkg-config/pkg-config-0.29.2.sh
@@ -6,7 +6,7 @@ get https://pkg-config.freedesktop.org/releases/${P}.tar.gz
 cd "${T}"
 
 # configure & build
-doconf --enable-languages=c,c++ --disable-bootstrap --disable-libmpx --with-system-zlib
+doconf --enable-languages=c,c++ --disable-bootstrap --disable-libmpx --with-system-zlib --with-pc-path=/usr/share/pkgconfig
 
 make
 make install DESTDIR="${D}"
