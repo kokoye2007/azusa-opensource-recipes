8f98c09563c52c9ac3590c925b394fafcaa487a7
Mark Karpeles <magicaltux@gmail.com>
2019-09-13 12:31:36 +0900

    small fixes for pam (still not compiling)

--- sys-libs/pam/pam-1.3.1.sh
+++ sys-libs/pam/pam-1.3.1.sh
@@ -6,10 +6,10 @@ get https://github.com/linux-pam/linux-pam/releases/download/v${PV}/Linux-PAM-${
 echo "Compiling Linux-PAM-${PV} ..."
 cd "${T}"
 
+export TIRPC_CFLAGS="-I/pkg/main/net-libs.libtirpc.dev/include/tirpc"
+
 # configure & build
-${CHPATH}/Linux-PAM-${PV}/configure --prefix=/usr --sysconfdir=/etc --exec-prefix=/pkg/main/${PKG}.core.${PVR} \
---includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR} --datarootdir=/pkg/main/${PKG}.core.${PVR}/share \
---mandir=/pkg/main/${PKG}.doc.${PVR}/man --docdir=/pkg/main/${PKG}.doc.${PVR}/doc
+doconf --enable-securedir=/etc/security
 
 make
 make install DESTDIR="${D}"
