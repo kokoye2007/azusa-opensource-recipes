b84713b5f2d00deef94dcf716e880edd3ad72c74
Mark Karpeles <magicaltux@gmail.com>
2019-09-17 09:37:22 +0900

    glibc: fix ldd

--- sys-libs/glibc/glibc-2.29.sh
+++ sys-libs/glibc/glibc-2.29.sh
@@ -16,6 +16,10 @@ cd "${D}"
 
 # move some stuff
 mv etc "pkg/main/${PKG}.core.${PVR}"
+
+# fix: create a symlink to libs/lib64 in core so ldd works
+ln -snf /pkg/main/sys-libs.glibc.libs.${PVR}/lib64 "pkg/main/${PKG}.core.${PVR}/lib64"
+
 #mv sbin/* "pkg/main/${PKG}.core.${PVR}/sbin"
 #mv lib* "pkg/main/${PKG}.libs.${PVR}"
 #mv var "pkg/main/${PKG}.core.${PVR}"
