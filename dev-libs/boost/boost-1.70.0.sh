72fe2c459ebf60238a16eaa4d21caf3a571bb755
Mark Karpeles <magicaltux@gmail.com>
2019-09-17 09:46:57 +0900

    boost: fix typo

--- dev-libs/boost/boost-1.70.0.sh
+++ dev-libs/boost/boost-1.70.0.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source "../../common/init.sh"
 
-get https://dl.bintray.com/boostorg/release/${PV}/source/boost_1_70_0.tar.bz2
+get https://dl.bintray.com/boostorg/release/${PV}/source/boost_${PV//./_}.tar.bz2
 
 cd "boost_1_70_0"
 
@@ -9,6 +9,6 @@ cd "boost_1_70_0"
 ./bootstrap.sh --with-icu --prefix=/pkg/main/${PKG}.core.${PVR}
 
 ./b2 --ignore-site-config
-./b2 --ignore-site-config install --prefox="${D}/pkg/main/${PKG}.core.${PVR}"
+./b2 --ignore-site-config install --prefix="${D}/pkg/main/${PKG}.core.${PVR}"
 
 finalize
