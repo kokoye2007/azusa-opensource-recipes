3736af64260d9985634e5c5c68cdfc9c1fb90e35
Mark Karpeles <magicaltux@gmail.com>
2019-09-17 15:10:54 +0900

    add linux 5.2.15

---
+++ sys-kernel/linux/linux-5.2.15.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://cdn.kernel.org/pub/linux/kernel/v5.x/${P}.tar.xz
+
+cd ${P}
+
+cp $FILESDIR/config-${PVR} ./.config
+
+make -j4
+
+echo "Building dist..."
+FULLVER=`make -s kernelrelease`
+IMGFILE=`make -s image_name`
+
+mkdir -p "${D}/pkg/main/${PKG}.core.${PVR}"
+cp "$IMGFILE" "${D}/pkg/main/${PKG}.core.${PVR}/linux-${PVR}.img"
+cp ".config" "${D}/pkg/main/${PKG}.core.${PVR}/linux-${PVR}.config"
+make modules_install INSTALL_MOD_PATH="${D}/pkg/main/${PKG}.modules.${PVR}"
+make headers_install INSTALL_HDR_PATH="${D}/pkg/main/${PKG}.dev.${PVR}"
+
+finalize
