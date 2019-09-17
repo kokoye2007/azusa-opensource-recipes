63cb57fe1e66e9fe2d0b7961f0e27c62b4f681e5
Mark Karpeles <magicaltux@gmail.com>
2019-09-17 09:46:41 +0900

    merge base-root into symlink/makeroot

--- core/symlinks/files/makeroot.sh
+++ core/symlinks/files/makeroot.sh
@@ -2,6 +2,9 @@
 
 # script to build a root directory for Azusa
 
+# See: https://en.wikipedia.org/wiki/Filesystem_Hierarchy_Standard
+
+
 BASE="$1"
 
 if [ x"$BASE" = x -o ! -d "$BASE" ]; then
@@ -9,15 +12,20 @@ if [ x"$BASE" = x -o ! -d "$BASE" ]; then
 	exit 1
 fi
 
-for foo in root sys proc etc etc/ssl dev dev/pts dev/shm tmp pkg usr usr/azusa usr/share home var var/run var/log; do
+for foo in root sys proc etc etc/ssl dev dev/pts dev/shm tmp pkg usr usr/azusa usr/share home var var/run var/tmp boot mnt opt media; do
 	if [ ! -d "$BASE/$foo" ]; then
 		mkdir "$BASE/$foo"
 	fi
 done
 
 # fix modes for /tmp
-chmod 1777 "$BASE/tmp"
+chmod 01777 "$BASE/tmp" "$BASE/var/tmp"
+chmod 0750 "$BASE/root"
 
+# configure /var
+mkdir -p $BASE/var/{log,mail,spool,cache,lib/{color,misc,locate},lock}
+
+# TODO multilib: limit lib64 to x86_64
 for foo in bin sbin lib lib32 lib64; do
 	ln -snf "/pkg/main/core.symlinks/$foo" "$BASE/$foo"
 	ln -snf "/pkg/main/core.symlinks/$foo" "$BASE/usr/$foo"
