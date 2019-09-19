8d76af34419ad18860e191b5144d39eff65e1ec6
Mark Karpeles <magicaltux@gmail.com>
2019-09-19 12:15:41 +0900

    fix squashfs

--- sys-fs/squashfs-tools/squashfs-tools-4.4.sh
+++ sys-fs/squashfs-tools/squashfs-tools-4.4.sh
@@ -5,6 +5,9 @@ get https://github.com/plougher/squashfs-tools/archive/${PV}.tar.gz
 
 cd "${P}/squashfs-tools"
 
+# make sure -lz works
+export LDFLAGS="$(pkg-config zlib --libs-only-L)"
+
 make
 make install INSTALL_DIR="${D}/pkg/main/${PKG}.${PVR}/bin"
 
