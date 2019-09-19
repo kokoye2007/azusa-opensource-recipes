64d190d0d40fcb6bb3a0322f49765db5f3500f10
Mark Karpeles <magicaltux@gmail.com>
2019-09-19 10:23:04 +0900

    tweaks to make gcc workg

--- sys-devel/gcc/gcc-9.2.0.sh
+++ sys-devel/gcc/gcc-9.2.0.sh
@@ -12,10 +12,15 @@ GCC_VERSION=`gcc -dumpversion`
 GCC_MACHINE=`gcc -dumpmachine`
 export CXXFLAGS="-I/pkg/main/sys-devel.gcc.core.$GCC_VERSION/include/c++/$GCC_VERSION -I/pkg/main/sys-devel.gcc.core.$GCC_VERSION/include/c++/$GCC_VERSION/$GCC_MACHINE"
 
+# make sure gcc can find stuff like -lz
+export LDFLAGS="-L/pkg/main/core.symlinks/full/lib$LIB_SUFFIX"
+
 export LD_LIBRARY_PATH=/pkg/main/dev-libs.isl.libs/lib$LIB_SUFFIX:/pkg/main/dev-libs.mpfr.libs/lib$LIB_SUFFIX:/pkg/main/dev-libs.mpc.libs/lib$LIB_SUFFIX:/pkg/main/dev-libs.gmp.libs/lib$LIB_SUFFIX
 
 # configure & build
-doconf --enable-languages=c,c++ --disable-multilib --disable-bootstrap --disable-libmpx --with-system-zlib \
+callconf --prefix=/pkg/main/${PKG}.core.${PVR} --infodir=/pkg/main/${PKG}.doc.${PVR}/info --mandir=/pkg/main/${PKG}.doc.${PVR}/man --docdir=/pkg/main/${PKG}.doc.${PVR}/gcc \
+--with-gcc-major-version-only \
+--enable-languages=c,c++ --disable-multilib --disable-bootstrap --disable-libmpx --with-system-zlib \
 --with-mpfr-include=`realpath /pkg/main/dev-libs.mpfr.dev/include` --with-mpfr-lib=`realpath /pkg/main/dev-libs.mpfr.libs/lib$LIB_SUFFIX` \
 --with-mpc-include=`realpath /pkg/main/dev-libs.mpc.dev/include` --with-mpc-lib=`realpath /pkg/main/dev-libs.mpc.libs/lib$LIB_SUFFIX` \
 --with-gmp-include=`realpath /pkg/main/dev-libs.gmp.dev/include` --with-gmp-lib=`realpath /pkg/main/dev-libs.gmp.libs/lib$LIB_SUFFIX` \
