a78f936c66ad0be1fa866d496fa85813ff6c9152
Mark Karpeles <magicaltux@gmail.com>
2019-09-19 13:06:16 +0900

    fix util-linux-2.34

--- sys-apps/util-linux/util-linux-2.34.sh
+++ sys-apps/util-linux/util-linux-2.34.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+cd "$(dirname $0)"
 source "../../common/init.sh"
 
-if [ `id -u` -neq 0 ]; then
+if [ `id -u` -ne 0 ]; then
 	echo "This needs to be compiled as root because reasons"
 	exit 1
 fi
@@ -10,13 +11,13 @@ get https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.34/${P}.tar.xz
 
 cd "${T}"
 
-doconf --disable-chfn-chsh --disable-login --disable-nologin --disable-su --disable-setpriv --disable-runuser --disable-pylibmount --disable-static --without-python --without-systemd --without-systemdsystemunitdir
+# libdir needs to start with $prefix in order to avoid weird bugs
+
+doconf --libdir=/pkg/main/${PKG}.core.${PVR}/lib$LIB_SUFFIX \
+	--disable-chfn-chsh --disable-login --disable-nologin --disable-su --disable-setpriv --disable-runuser --disable-pylibmount --disable-static \
+	--without-python --without-systemd --without-systemdsystemunitdir usrlib_execdir="/pkg/main/${PKG}.libs.${PVR}/lib64"
 
 make
 make install DESTDIR="${D}"
 
-# fix location of some files
-mv -v "${D}/pkg/main/${PKG}.core.${PVR}/pkg/main/${PKG}.libs.${PVR}/lib$LIB_SUFFIX/"* "${D}/pkg/main/${PKG}.libs.${PVR}/lib$LIB_SUFFIX"
-rm -frv "${D}/pkg/main/${PKG}.core.${PVR}/pkg"
-
-finalize
+archive
