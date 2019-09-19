033dc261d9d063e18e8ab19d74ae26422aac725a
Mark Karpeles <magicaltux@gmail.com>
2019-09-19 13:45:34 +0900

    fix -lz link for git

--- dev-vcs/git/git-2.23.0.sh
+++ dev-vcs/git/git-2.23.0.sh
@@ -5,6 +5,8 @@ get https://mirrors.edge.kernel.org/pub/software/scm/git/${P}.tar.xz
 
 cd "${P}"
 
+export LDFLAGS="$(pkg-config zlib --libs-only-L)"
+
 # configure & build
 # NOTE: ncurses doesn't support --docdir
 doconf
