1ff0f4e23f77aed5b6d6bfbd0acd343dd6b8cb61
Mark Karpeles <magicaltux@gmail.com>
2019-09-20 23:27:26 +0900

    fix python 2.7.16 (seems ok)

---
+++ dev-lang/python-modules/python-modules-0.0.1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+source ../../common/init.sh
+
+# this will compile & generate python-modules package for each listed version of python
+PYTHON_VERSIONS="2.7.16"
+
+for PYTHON_VERSION in $PYTHON_VERSIONS; do
+	echo "Generating for $PYTHON_VERSION ..."
+
+	TARGET="${D}/pkg/main/${PKG}.${PYTHON_VERSION}"
+	mkdir -p "${TARGET}"
+
+	# generate path from /pkg/main/dev-lang.python.mod.${PYTHON_VERSION}
+
+	MODP="/pkg/main/dev-lang.python.mod.${PYTHON_VERSION}"
+
+	find "$MODP/" -type f -printf "%P\n" | while read foo; do
+		foo_dir=`dirname "$foo"`
+		if [ ! -d "$TARGET/$foo_dir" ]; then
+			mkdir -p "$TARGET/$foo_dir"
+		fi
+		ln -snf "$MODP/$foo" "$TARGET/$foo"
+	done
+done
+
+finalize
--- dev-lang/python/python-2.7.16.sh
+++ dev-lang/python/python-2.7.16.sh
@@ -7,17 +7,22 @@ cd "Python-${PV}"
 
 # ensure python can build its "bits" for the following packages
 PKGS="libffi expat ncurses openssl zlib sqlite3 readline"
-export CFLAGS="$(pkg-config --cflags $PKGS)"
-export LDFLAGS="$(pkg-config --libs-only-L $PKGS)"
+export CFLAGS="$(pkg-config --cflags-only-I $PKGS) -I/pkg/main/app-arch.bzip2.dev/include"
+export LDFLAGS="$(pkg-config --libs-only-L $PKGS) -L/pkg/main/app-arch.bzip2.libs/lib64"
 
-doconf --enable-shared --with-system-expat --with-system-ffi --with-ensurepip=install --enable-unicode=ucs4 --with-cxx-main=g++ --enable-optimizations
+callconf --prefix="/pkg/main/dev-lang.python-modules.${PV}" --exec-prefix="/pkg/main/${PKG}.core.${PVR}" --sysconfdir=/etc --localstatedir=/var --includedir="\${exec_prefix}/include" --datarootdir="\${exec_prefix}/share" \
+	--infodir="/pkg/main/${PKG}.doc.${PVR}/info" --mandir="/pkg/main/${PKG}.doc.${PVR}/man" --docdir="/pkg/main/${PKG}.doc.${PVR}" \
+	--enable-shared --with-system-expat --with-system-ffi --with-ensurepip=install --enable-unicode=ucs4 --with-cxx-main=g++ --enable-optimizations
 
 make
 make install DESTDIR="${D}"
 
+# move modules installed to exec-prefix back to prefix
+mv "${D}/pkg/main/dev-lang.python-modules.${PV}" "${D}/pkg/main/${PKG}.mod.${PVR}"
+
 # pyconfig.h is installed in the wrong place
-mv -v "${D}/pkg/main/${PKG}.core.${PVR}/include/python2.7"/* "${D}/pkg/main/${PKG}.dev.${PVR}/include/python2.7"
-rm -fr "${D}/pkg/main/${PKG}.core.${PVR}/include"
-ln -snfv "/pkg/main/${PKG}.dev.${PVR}/include" "${D}/pkg/main/${PKG}.core.${PVR}/include"
+#mv -v "${D}/pkg/main/${PKG}.core.${PVR}/include/python2.7"/* "${D}/pkg/main/${PKG}.dev.${PVR}/include/python2.7"
+#rm -fr "${D}/pkg/main/${PKG}.core.${PVR}/include"
+#ln -snfv "/pkg/main/${PKG}.dev.${PVR}/include" "${D}/pkg/main/${PKG}.core.${PVR}/include"
 
 finalize
