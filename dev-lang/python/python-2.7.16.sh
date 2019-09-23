220dc9a9e5b905599af846e6dd2a08b618d4aa74
Mark Karpeles <magicaltux@gmail.com>
2019-09-24 08:13:21 +0900

    add libgdbm

--- dev-lang/python/python-2.7.16.sh
+++ dev-lang/python/python-2.7.16.sh
@@ -6,8 +6,8 @@ get https://www.python.org/ftp/python/${PV}/Python-${PV}.tar.xz
 cd "Python-${PV}"
 
 # ensure python can build its "bits" for the following packages
-CFLAGS="-I/pkg/main/app-arch.bzip2.dev/include"
-LDFLAGS="-L/pkg/main/app-arch.bzip2.libs/lib64"
+CFLAGS="-I/pkg/main/app-arch.bzip2.dev/include -I/pkg/main/sys-libs.gdbm.dev/include"
+LDFLAGS="-L/pkg/main/app-arch.bzip2.libs/lib64 -L/pkg/main/sys-libs.gdbm.libs/lib64"
 importpkg libffi expat ncurses openssl zlib sqlite3 readline
 
 callconf --prefix="/pkg/main/dev-lang.python-modules.${PV}" --exec-prefix="/pkg/main/${PKG}.core.${PVR}" --sysconfdir=/etc --localstatedir=/var --includedir="\${exec_prefix}/include" --datarootdir="\${exec_prefix}/share" \
