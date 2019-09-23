377f8aabc05dae526de93944a82fc8ab786ffff3
Mark Karpeles <magicaltux@gmail.com>
2019-09-23 11:15:10 +0900

    move python setup invokation to its own function

--- common/python.sh
+++ common/python.sh
@@ -17,3 +17,20 @@ disable_ez_setup() {
         echo "${stub}" > distribute_setup.py || die
     fi
 }
+
+pythonsetup() {
+	if [ ! -d /.pkg-main-rw ]; then
+		echo "This needs to be built in Azusa Build env"
+		exit
+	fi
+
+	mkdir -p "${D}/pkg/main"
+
+	# perform install for all relevant versions of python
+	for PYTHON_VERSION in $PYTHON_VERSIONS; do
+		"/pkg/main/dev-lang.python.core.${PYTHON_VERSION}/bin/python${PYTHON_VERSION:0:1}" setup.py install
+
+		# fetch the installed module from /.pkg-main-rw/
+		mv "/.pkg-main-rw/dev-lang.python-modules.${PYTHON_VERSION}".* "${D}/pkg/main/${PKG}.mod.${PVR}.py${PYTHON_VERSION}"
+	done
+}
--- dev-python/setuptools/setuptools-41.2.0.sh
+++ dev-python/setuptools/setuptools-41.2.0.sh
@@ -4,20 +4,7 @@ source ${ROOTDIR}/common/python.sh
 
 get https://pypi.org/packages/source/${PN:0:1}/${PN}/${P}.zip
 
-if [ ! -d /.pkg-main-rw ]; then
-	echo "This needs to be built in Azusa Build env"
-	exit
-fi
-
 cd "${P}"
-mkdir -p "${D}/pkg/main"
 
-# perform install for all relevant versions of python
-for PYTHON_VERSION in $PYTHON_VERSIONS; do
-	"/pkg/main/dev-lang.python.core.${PYTHON_VERSION}/bin/python${PYTHON_VERSION:0:1}" setup.py install
-
-	# fetch the installed module from /.pkg-main-rw/
-	mv "/.pkg-main-rw/dev-lang.python-modules.${PYTHON_VERSION}".* "${D}/pkg/main/${PKG}.mod.${PVR}.py${PYTHON_VERSION}"
-done
-
+pythonsetup
 archive
