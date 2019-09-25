9d0973f2bc7de24f1e57389a530f8306e4382e94
Mark Karpeles <magicaltux@gmail.com>
2019-09-26 00:41:41 +0900

    update python3 build

--- dev-lang/python/python-3.7.3.sh
+++ dev-lang/python/python-3.7.3.sh
@@ -6,13 +6,13 @@ get https://www.python.org/ftp/python/${PV}/Python-${PV}.tar.xz
 cd "Python-${PV}"
 
 # ensure python can build its "bits" for the following packages
-CFLAGS="-I/pkg/main/app-arch.bzip2.dev/include"
-LDFLAGS="-L/pkg/main/app-arch.bzip2.libs/lib64"
+CFLAGS="-I/pkg/main/app-arch.bzip2.dev/include -I/pkg/main/sys-libs.gdbm.dev/include"
+LDFLAGS="-L/pkg/main/app-arch.bzip2.libs/lib64 -L/pkg/main/sys-libs.gdbm.libs/lib64"
 importpkg libffi expat ncurses openssl zlib sqlite3 readline liblzma
 
 callconf --prefix="/pkg/main/dev-lang.python-modules.${PV}" --exec-prefix="/pkg/main/${PKG}.core.${PVR}" --sysconfdir=/etc --localstatedir=/var --includedir="\${exec_prefix}/include" --datarootdir="\${exec_prefix}/share" \
 	--infodir="/pkg/main/${PKG}.doc.${PVR}/info" --mandir="/pkg/main/${PKG}.doc.${PVR}/man" --docdir="/pkg/main/${PKG}.doc.${PVR}" \
-	--enable-shared --with-system-expat --with-system-ffi --with-ensurepip=install --enable-unicode=ucs4 --enable-optimizations
+	--enable-shared --with-system-expat --with-system-ffi --enable-optimizations --with-computed-gotos --with-dbmliborder=gdbm:bdb --with-libc= --enable-loadable-sqlite-extensions --without-ensurepip
 
 make
 make install DESTDIR="${D}"
