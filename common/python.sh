62b0ba3ea3a30d41d6646559d1c05a0c15e81eca
Mark Karpeles <magicaltux@gmail.com>
2019-09-26 23:51:22 +0900

    add opencv 3.4.7

--- common/python.sh
+++ common/python.sh
@@ -3,7 +3,7 @@
 # currently active python versions (for modules, etc)
 PYTHON_VERSIONS="2.7.16 3.7.3"
 
-PYTHON_MODS="dev-python/setuptools dev-python/pip dev-util/gyp dev-util/meson dev-python/pycairo dev-python/pygobject"
+PYTHON_MODS="dev-python/setuptools dev-python/pip dev-util/gyp dev-util/meson dev-python/pycairo dev-python/pygobject dev-python/numpy"
 
 # (from gentoo, to clean/remove/reuse?)
 # Stub out ez_setup.py and distribute_setup.py to prevent packages
