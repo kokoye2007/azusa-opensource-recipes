87ce29e4f5e6df1be61994b0234771794f94fb6f
Mark Karpeles <magicaltux@gmail.com>
2019-09-28 03:29:30 +0900

    rhash: link against system openssl, etc

--- app-crypt/rhash/rhash-1.3.8.sh
+++ app-crypt/rhash/rhash-1.3.8.sh
@@ -4,11 +4,13 @@ source "../../common/init.sh"
 get https://download.sourceforge.net/${PN}/${P}-src.tar.gz
 acheck
 
+importpkg dev-libs/openssl
+
 cd */
 
-doconflight
+doconflight --disable-openssl-runtime --disable-static --enable-lib-shared --enable-gettext --enable-openssl  --extra-cflags="$CPPFLAGS" --extra-ldflags="$LDFLAGS"
 
 make
-make install DESTDIR="${D}"
+make install{,-lib-headers,-pkg-config} install-lib-so-link DESTDIR="${D}"
 
 finalize
