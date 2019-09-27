b03b00397b7353b40269ce254deb931176e3e7da
Mark Karpeles <magicaltux@gmail.com>
2019-09-27 09:14:07 +0900

    fix libtheora run cmdline

--- media-libs/libtheora/libtheora-1.1.1.sh
+++ media-libs/libtheora/libtheora-1.1.1.sh
@@ -5,7 +5,7 @@ get http://downloads.xiph.org/releases/theora/${P}.tar.bz2
 
 cd "${T}"
 
-doconf
+doconf --disable-static --disable-oggtest --disable-vorbistest --disable-sdltest --disable-examples
 
 make
 make install DESTDIR="${D}"
