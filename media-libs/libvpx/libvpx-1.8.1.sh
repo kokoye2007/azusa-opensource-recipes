9ab08bcf03b3d84e5f814d4239c05800149d0107
Mark Karpeles <magicaltux@gmail.com>
2019-09-27 10:51:06 +0900

    fix libvpx configure

--- media-libs/libvpx/libvpx-1.8.1.sh
+++ media-libs/libvpx/libvpx-1.8.1.sh
@@ -7,7 +7,7 @@ acheck
 
 cd "${T}"
 
-doconflight
+doconflight --disable-examples --enable-pic --enable-vp9-highbitdepth --enable-vp8 --enable-vp9 --enable-postproc --enable-vp9-postproc --enable-runtime-cpu-detect --enable-shared --disable-static --enable-multi-res-encoding --enable-vp9-temporal-denoising --enable-webm-io --enable-libyuv
 
 make
 make install DESTDIR="${D}"
