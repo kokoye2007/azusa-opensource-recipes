f9fbd1f2df8167400fe469471da4c77ff45ed3a4
Mark Karpeles <magicaltux@gmail.com>
2019-09-28 20:32:32 +0900

    fixec cmake & libarchive, llvm

--- app-arch/libarchive/libarchive-3.4.0.sh
+++ app-arch/libarchive/libarchive-3.4.0.sh
@@ -2,10 +2,13 @@
 source "../../common/init.sh"
 
 get https://github.com/libarchive/libarchive/releases/download/v${PV}/${P}.tar.gz
+acheck
 
 cd "${T}"
 
-doconf --disable-static
+importpkg app-arch/bzip2 dev-libs/lzo
+
+doconf --disable-static --with-lzo2
 
 make
 make install DESTDIR="${D}"
--- sys-devel/clang/clang-9.0.0.sh
+++ sys-devel/clang/clang-9.0.0.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 source "../../common/init.sh"
 
-get http://releases.llvm.org/${PV}/${P}.src.tar.xz
+get http://releases.llvm.org/${PV}/cfe-${PV}.src.tar.xz
 acheck
 
 cd "${T}"
 
 # see http://llvm.org/docs/CMake.html
 
-cmake ${CHPATH}/${P}.src -DCMAKE_INSTALL_PREFIX=/pkg/main/${PKG}.core.${PVR} -DLLVM_ENABLE_TERMINFO=ON \
+# make it possible to run llvm tools within a non-azusa linux (TEMP)
+export LLVM_DIR=/pkg/main/sys-devel.llvm.dev.${PV}/lib64/cmake/llvm
+
+cmake ${CHPATH}/cfe-${PV}.src -DCMAKE_INSTALL_PREFIX=/pkg/main/${PKG}.dev.${PVR} -DLLVM_ENABLE_TERMINFO=ON \
 	-DLLVM_ENABLE_LIBXML2=ON -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_APPEND_VC_REV=OFF -DWITH_POLLY=OFF -DLLVM_INSTALL_UTILS=ON \
 	-DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON \
 	-DLLVM_ENABLE_FFI=ON -DFFI_INCLUDE_DIR=`realpath /pkg/main/dev-libs.libffi.dev/include` -DFFI_LIBRARY_DIR=`realpath /pkg/main/dev-libs.libffi.libs/lib64`
@@ -16,12 +19,8 @@ cmake ${CHPATH}/${P}.src -DCMAKE_INSTALL_PREFIX=/pkg/main/${PKG}.core.${PVR} -DL
 #cmake --build .
 make -j8
 
-cmake -DCMAKE_INSTALL_PREFIX="${D}/pkg/main/${PKG}.core.${PVR}" -P cmake_install.cmake
+cmake -DCMAKE_INSTALL_PREFIX="${D}/pkg/main/${PKG}.dev.${PVR}" -P cmake_install.cmake
 
 cd "${D}"
-
-# fix dev pkg
-mkdir -p "pkg/main/${PKG}.dev.${PVR}"
-ln -s "/pkg/main/${PKG}.core.${PVR}/bin" "pkg/main/${PKG}.dev.${PVR}/bin"
 
 finalize
