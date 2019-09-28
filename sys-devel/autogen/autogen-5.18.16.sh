01d41b58d497bac6ae7cc12aa796ae7e67dcb63e
Mark Karpeles <magicaltux@gmail.com>
2019-09-28 17:24:13 +0900

    fix autogen

--- sys-devel/autogen/autogen-5.18.16.sh
+++ sys-devel/autogen/autogen-5.18.16.sh
@@ -4,9 +4,12 @@ source "../../common/init.sh"
 get https://ftp.gnu.org/gnu/${PN}/rel${PV}/${P}.tar.xz
 acheck
 
+cd "${P}"
+sed -i -e '/def_bf/s/MAXNAMELEN/MAXNAMELEN+4/' getdefs/getdefs.c
+
 cd "${T}"
 
-doconf --disable-static
+doconf --disable-static --disable-dependency-tracking
 
 make
 make install DESTDIR="${D}"
