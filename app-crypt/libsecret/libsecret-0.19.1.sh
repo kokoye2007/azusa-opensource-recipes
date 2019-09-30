ac59e1ab47609dc6a3c7d38ba79c77c99b282620
Mark Karpeles <magicaltux@gmail.com>
2019-10-01 07:55:52 +0900

    add libsecret & meson-0.51.2

---
+++ app-crypt/libsecret/libsecret-0.19.1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get http://ftp.gnome.org/pub/gnome/sources/${PN}/${PV%.*}/${P}.tar.xz
+acheck
+
+cd "${P}"
+
+sed '/valgrind/d' -i egg/egg-testing.c
+
+cd "${T}"
+
+meson "${CHPATH}/${P}" --prefix=/pkg/main/${PKG}.core.${PVR} -Dgtk_doc=false
+
+ninja
+DESTDIR="${D}" ninja install
+
+finalize
