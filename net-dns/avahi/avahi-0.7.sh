b4060f3fd620c90eee737ea3815f4a219f42c62d
Mark Karpeles <magicaltux@gmail.com>
2019-10-01 08:35:01 +0900

    cups wip

--- net-dns/avahi/avahi-0.7.sh
+++ net-dns/avahi/avahi-0.7.sh
@@ -2,10 +2,11 @@
 source "../../common/init.sh"
 
 get http://avahi.org/download/${P}.tar.gz
+acheck
 
 cd "${T}"
 
-doconf --disable-qt3 --disable-qt4 --disable-gtk --disable-gtk3 --disable-static 
+doconf --sysconfdir=/etc --localstatedir=/var --disable-qt3 --disable-qt4 --disable-gtk --disable-gtk3 --disable-static --disable-mono --disable-monodoc --disable-python --with-distro=none --with-systemdsystemunitdir=no
 
 make
 make install DESTDIR="${D}"
---
+++ net-print/cups-filters/cups-filters-1.25.6.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://www.openprinting.org/download/cups-filters/${P}.tar.xz
+acheck
+
+cd "${T}"
+
+importpkg libjpeg libtiff-4 net-dns/avahi
+
+# TODO enable avahi
+doconf --sysconfdir=/etc --localstatedir=/var --without-rcdir --disable-static --disable-avahi
+
+make
+make install DESTDIR="${D}"
+
+finalize
