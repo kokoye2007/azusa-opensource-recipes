7fc62bf0401bc0e89de8bdc0d85399df4739e6e4
Mark Karpeles <magicaltux@gmail.com>
2019-10-02 08:40:50 +0900

    script for ntp (pending perl modules)

---
+++ net-misc/ntp/ntp-4.2.8p13.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-${PV%.*}/${P}.tar.gz
+acheck
+
+cd "${P}"
+
+sed -e 's/"(\\S+)"/"?([^\\s"]+)"?/' -i scripts/update-leap/update-leap.in
+
+cd "${T}"
+
+importpkg zlib
+
+doconf CFLAGS="-O2 -g -fPIC" --sysconfdir=/etc --enable-linuxcaps --with-lineeditlibs=readline
+
+make
+make install DESTDIR="${D}"
+
+finalize
