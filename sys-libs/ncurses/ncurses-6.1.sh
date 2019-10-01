99be48c8522056f7a524fb44f3253af1d0729775
Mark Karpeles <magicaltux@gmail.com>
2019-10-01 23:58:10 +0900

    fix ncurses pkgconfig install

--- sys-libs/ncurses/ncurses-6.1.sh
+++ sys-libs/ncurses/ncurses-6.1.sh
@@ -13,7 +13,7 @@ cd "${T}"
 # without widec
 callconf --prefix=/pkg/main/${PKG}.core.${PVR} --sysconfdir=/etc \
 --includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR}/lib$LIB_SUFFIX --datarootdir=/pkg/main/${PKG}.core.${PVR}/share \
---mandir=/pkg/main/${PKG}.doc.${PVR}/man \
+--mandir=/pkg/main/${PKG}.doc.${PVR}/man --with-pkg-config --with-pkg-config-libdir=/pkg/main/${PKG}.dev.${PVR}/pkgconfig \
 --disable-widec --enable-pc-files --with-shared --without-normal --without-debug --with-termlib
 
 make
@@ -24,15 +24,13 @@ make distclean
 # with widec
 callconf --prefix=/pkg/main/${PKG}.core.${PVR} --sysconfdir=/etc \
 --includedir=/pkg/main/${PKG}.dev.${PVR}/include --libdir=/pkg/main/${PKG}.libs.${PVR}/lib$LIB_SUFFIX --datarootdir=/pkg/main/${PKG}.core.${PVR}/share \
---mandir=/pkg/main/${PKG}.doc.${PVR}/man \
+--mandir=/pkg/main/${PKG}.doc.${PVR}/man --with-pkg-config --with-pkg-config-libdir=/pkg/main/${PKG}.dev.${PVR}/pkgconfig \
 --enable-widec --enable-pc-files --with-shared --without-normal --without-debug --with-termlib
 
 make
 make install DESTDIR="${D}"
 
 cd "${D}"
-
-mv usr/share/pkgconfig "pkg/main/${PKG}.dev.${PVR}/"
 
 #cd "pkg/main/${PKG}.libs.${PVR}/lib64"
 #for lib in ncurses form panel menu ; do
