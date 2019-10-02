0914487050ee65f75c10eccbf75e866a18025d67
Mark Karpeles <magicaltux@gmail.com>
2019-10-02 11:32:44 +0900

    new symlinks!

--- core/symlinks/files/build.sh
+++ core/symlinks/files/build.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 set -e
 
-DIRS_core="bin sbin info share/gir-1.0"
-DIRS_dev="include cmake pkgconfig"
 MULTILIB=yes
 
 cd $1
-mkdir -p $DIRS_core include
-mkdir etc etc/ssl
+mkdir -p bin sbin info share/gir-1.0 include etc etc/ssl include full/include
 ln -snf /pkg/main/app-misc.ca-certificates/etc/ssl/certs etc/ssl/certs
 
 if [ $MULTILIB = yes ]; then
@@ -17,8 +14,8 @@ if [ $MULTILIB = yes ]; then
 	LIBS="lib64 lib32 lib"
 	LIB=lib64
 
-	ln -s `realpath /pkg/main/sys-libs.glibc.libs`/lib64/ld-linux-x86-64.so.2 lib64
-	ln -s `realpath /pkg/main/sys-libs.glibc.libs`/lib64/*.o lib64
+	#ln -s `realpath /pkg/main/sys-libs.glibc.libs`/lib64/ld-linux-x86-64.so.2 lib64
+	cp -rsfT `realpath /pkg/main/sys-libs.glibc.libs/lib64` lib64
 else
 	LIBS=lib
 	LIB=lib
@@ -28,7 +25,15 @@ mkdir -p "$LIB/cmake" "$LIB/pkgconfig"
 ln -snf "$LIB/cmake" cmake
 ln -snf "$LIB/pkgconfig" pkgconfig
 
+# build includes
+echo "Building include folder..."
+for foo in sys-libs/glibc sys-libs/libcxx; do
+	# use realpath to resolve path to full path with version
+	cp -rsfT $(realpath /pkg/main/${foo/\//.}.dev/include) ./include
+done
+
 for pn in $(apkg-ctrl apkgdb/main?action=list | grep -v busybox | grep -v symlinks); do
+	echo -ne "\rScanning: $pn\033[K"
 	p=/pkg/main/${pn}
 	t=`echo "$pn" | cut -d. -f3`
 
@@ -42,50 +47,44 @@ for pn in $(apkg-ctrl apkgdb/main?action=list | grep -v busybox | grep -v symlin
 		continue
 	fi
 
-	DIRS=""
-
 	case $t in
 		core)
-			DIRS="$DIRS_core"
+			for foo in bin sbin info share/gir-1.0; do
+				if [ -d "${p}/${foo}" ]; then
+					cp -rsfT "${p}/${foo}" "${foo}"
+				fi
+			done
 			;;
 		dev)
-			DIRS="$DIRS_dev"
+			for foo in cmake pkgconfig; do
+				if [ -d "${p}/${foo}" ]; then
+					cp -rsfT "${p}/${foo}" "$LIB/${foo}"
+				fi
+			done
+
+			# generate full/include
+			if [ -d "${p}/include" ]; then
+				cp -rsfT "${p}/include" "full/include"
+			fi
+			;;
+		doc)
+			if [ -d "${p}/man" ]; then
+				cp -rsfT "${p}/man" man
+			fi
+			;;
+		libs)
+			for foo in $LIBS; do
+				if [ -d "${p}/$foo" -a ! -L "${p}/$foo" ]; then
+					echo "${p}/$foo" >>etc/ld.so.conf.tmp
+					# generate symlinks for full/lib64
+					cp -rsfT "${p}/$foo" full/$foo/
+				fi
+			done
 			;;
 	esac
-
-	if [ x"$DIRS" != x ]; then
-		for foo in $DIRS; do
-			if [ -d "${p}/$foo" -a ! -L "${p}/$foo" ]; then
-				cp -rsf "${p}/$foo"/* "$foo" || true
-			fi
-		done
-	fi
-
-	# check if third element of package name is "libs"
-	if [ x"$t" = x"libs" ]; then
-		for foo in $LIBS; do
-			if [ -d "${p}/$foo" -a ! -L "${p}/$foo" ]; then
-				echo "${p}/$foo" >>etc/ld.so.conf.tmp
-				# generate symlinks for full/lib64
-				for bar in "${p}/$foo"/*.so*; do
-					if [ -f "$bar" ]; then
-						ln -snf "$bar" full/$foo
-					fi
-				done
-			fi
-		done
-	fi
-
-	if [ -d "${p}/man" ]; then
-		for foo in "${p}/man"/*; do
-			m=`basename "$foo"`
-			mkdir -p "man/$m"
-			ln -snf "$foo"/* "man/$m/"
-		done
-	fi
-
-	echo $p
+	# TODO: fonts, sgml
 done
+echo
 
 realpath /pkg/main/sys-libs.glibc.libs/$LIB >>etc/ld.so.conf.tmp
 tac etc/ld.so.conf.tmp >etc/ld.so.conf
