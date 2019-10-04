d871b1cb6b0374d6750889acbbbede338d4e0ed7
Mark Karpeles <magicaltux@gmail.com>
2019-10-04 19:38:25 +0900

    fix perl module compile

--- common/perl.sh
+++ common/perl.sh
@@ -5,3 +5,26 @@ PERL_VERSIONS="5.30.0"
 
 PERL_MODS=""
 
+perlsetup() {
+	if [ ! -d /.pkg-main-rw ]; then
+		echo "This needs to be built in Azusa Build env"
+		exit
+	fi
+
+	mkdir -p "${D}/pkg/main"
+
+	# perform install for all relevant versions of python
+	for PERL_VERSION in $PERL_VERSIONS; do
+		"/pkg/main/dev-lang.perl.core.${PERL_VERSION}/bin/perl" Makefile.PL
+		make
+		make install
+
+		# fetch the installed module from /.pkg-main-rw/
+		mv "/.pkg-main-rw/dev-lang.perl-modules.core.${PERL_VERSION}"* "${D}/pkg/main/${PKG}.mod.${PVR}.perl${PERL_VERSION}"
+		if [ -d "/.pkg-main-rw/dev-lang.perl-modules.doc.${PERL_VERSION}"* ]; then
+			mv "/.pkg-main-rw/dev-lang.perl-modules.doc.${PERL_VERSION}"* "${D}/pkg/main/${PKG}.doc.${PVR}.perl${PERL_VERSION}"
+		fi
+
+		make distclean
+	done
+}
--- dev-perl/Archive-Zip/Archive-Zip-1.66.sh
+++ dev-perl/Archive-Zip/Archive-Zip-1.66.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 source "../../common/init.sh"
+inherit perl
 
 get https://www.cpan.org/authors/id/P/PH/PHRED/${P}.tar.gz
 acheck
 
 cd "${P}"
 
-perl Makefile.PL
-make
-
-make install
-
+perlsetup
 finalize
