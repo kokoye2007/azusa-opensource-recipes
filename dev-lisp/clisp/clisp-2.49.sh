776ed4d4828cf26138d0af074cebd864d5555177
Mark Karpeles <magicaltux@gmail.com>
2019-10-05 17:16:49 +0900

    attempt at getting clisp workign (didnt work)

---
+++ dev-lisp/clisp/clisp-2.49.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://ftp.gnu.org/gnu/${P}/latest/${P}.tar.bz2
+acheck
+
+cd "${P}"
+
+sed -i -e '/socket/d' -e '/"streams"/d' tests/tests.lisp
+patch -Np1 -i "$FILESDIR/clisp-2.49-readline7_fixes-1.patch"
+
+importpkg dev-libs/libsigsegv ncurses libffi
+
+cd "${T}"
+
+doconflight --srcdir="${CHPATH}/${P}" --with-libsigsegv-prefix=/pkg/main/dev-libs.libsigsegv.dev --with-libffcall-prefix=/pkg/main/dev-libs.libffcall.dev
+
+# increase stack size during build
+ulimit -s 16384
+make -j1
+make install DESTDIR="${D}"
+
+finalize
