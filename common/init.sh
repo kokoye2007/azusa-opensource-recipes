5499c043e76c74edbe2b4378a003ac2637422b3c
Mark Karpeles <magicaltux@gmail.com>
2019-10-09 13:43:04 +0900

    mplayer compiles

--- common/init.sh
+++ common/init.sh
@@ -124,7 +124,7 @@ finalize() {
 		if [ -d "pkg/main/${PKG}.core.${PVR}/$foo" -a ! -L "pkg/main/${PKG}.core.${PVR}/$foo" ]; then
 			echo "Moving core $foo directory to libs"
 			mkdir -p "pkg/main/${PKG}.libs.${PVR}/$foo"
-			mv -v "pkg/main/${PKG}.core.${PVR}/$foo"/* "pkg/main/${PKG}.libs.${PVR}/$foo"
+			mv -v "pkg/main/${PKG}.core.${PVR}/$foo"/* "pkg/main/${PKG}.libs.${PVR}/$foo" || true
 			rm -frv "pkg/main/${PKG}.core.${PVR}/$foo"
 			ln -sv "/pkg/main/${PKG}.libs.${PVR}/$foo" "pkg/main/${PKG}.core.${PVR}/$foo"
 		fi
--- media-video/mplayer/mplayer-1.4.sh
+++ media-video/mplayer/mplayer-1.4.sh
@@ -2,10 +2,17 @@
 source "../../common/init.sh"
 
 get http://www.mplayerhq.hu/MPlayer/releases/MPlayer-${PV}.tar.xz
+acheck
 
-cd "${T}"
+cd "MPlayer-${PV}"
 
-doconf
+# fix attempts to access ffmpeg includes
+rm -fr ffmpeg
+ln -snfT /pkg/main/media-video.ffmpeg.dev/include ffmpeg
+
+importpkg media-video/ffmpeg
+
+doconflight --confdir=/etc/mplayer --disable-svga --disable-svgalib_helper --disable-ass-internal --disable-arts --disable-directfb --disable-kai --disable-libopus --disable-libilbc --disable-xvmc --enable-networking --disable-gui --disable-vesa --disable-ffmpeg_a --extra-cflags="$CPPFLAGS"
 
 make
 make install DESTDIR="${D}"
