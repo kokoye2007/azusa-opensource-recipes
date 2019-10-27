56fa7512bc3c976637a753d53850c46e9acf9365
Mark Karpeles <magicaltux@gmail.com>
2019-10-28 07:39:38 +0900

    adding azusa.yaml for dbus

--- sys-apps/dbus/dbus-1.12.16.sh
+++ sys-apps/dbus/dbus-1.12.16.sh
@@ -2,10 +2,11 @@
 source "../../common/init.sh"
 
 get https://dbus.freedesktop.org/releases/dbus/${P}.tar.gz
+acheck
 
 cd "${T}"
 
-doconf --sysconfdir=/etc --localstatedir=/var --enable-user-session --disable-xml-docs --disable-static --with-systemduserunitdir=no --with-systemdsystemunitdir=no --with-console-auth-dir=/var/run/console --with-system-pid-file=/var/run/dbus/pid --with-system-socket=/var/run/dbus/system_bus_socket
+doconf --sysconfdir=/etc/dbus-1 --enable-user-session --disable-xml-docs --disable-static --with-systemduserunitdir=no --with-systemdsystemunitdir=no --with-console-auth-dir=/var/run/console --with-system-pid-file=/var/run/dbus/pid --with-system-socket=/var/run/dbus/system_bus_socket
 
 make
 make install DESTDIR="${D}"
