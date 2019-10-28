2620a8295de9dc7aca86c46042dedf9d2d6ae1a0
Mark Karpeles <magicaltux@gmail.com>
2019-10-28 19:38:44 +0900

    add compile options

--- sys-apps/shadow/shadow-4.7.sh
+++ sys-apps/shadow/shadow-4.7.sh
@@ -2,11 +2,12 @@
 source "../../common/init.sh"
 
 get https://github.com/shadow-maint/shadow/releases/download/${PV}/${P}.tar.xz
+acheck
 
 cd "${T}"
 
 # configure & build
-doconf
+doconf --without-group-name-max-length --without-tcb --enable-shared=no --enable-static=yes --with-libpam --with-libcrack --with-acl --enable-nls
 
 make
 make install DESTDIR="${D}"
