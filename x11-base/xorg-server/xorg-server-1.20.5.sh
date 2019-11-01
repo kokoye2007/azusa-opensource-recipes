635c2e528bdeae35f090d826f6d375a7cb4bd810
Mark Karpeles <magicaltux@gmail.com>
2019-11-02 01:44:11 +0900

    xorg: move modules to special dir

--- x11-base/xorg-server/xorg-server-1.20.5.sh
+++ x11-base/xorg-server/xorg-server-1.20.5.sh
@@ -2,13 +2,18 @@
 source "../../common/init.sh"
 
 get https://www.x.org/pub/individual/xserver/${P}.tar.bz2
+acheck
 
 cd "${T}"
 
-doconf --localstatedir=/var --enable-glamor --enable-suid-wrapper --disable-systemd-logind --with-xkb-output=/var/lib/xkb --enable-dmx --enable-kdrive
-# --enable-install-setuid
+doconf --enable-glamor --enable-suid-wrapper --disable-systemd-logind --with-xkb-output=/var/lib/xkb --enable-dmx --enable-kdrive --enable-install-setuid
 
 make
 make install DESTDIR="${D}"
+
+# move xorg modules
+mkdir -vp "${D}/pkg/main/${PKG}.mod.${PVR}/xorg"
+mv -vT "${D}/pkg/main/${PKG}.libs.${PVR}/lib$LIB_SUFFIX/xorg/modules" "${D}/pkg/main/${PKG}.mod.${PVR}/lib$LIB_SUFFIX/xorg/modules"
+ln -snfTv "/pkg/main/${PKG}-modules.libs.${PVR}/lib$LIB_SUFFIX/xorg/modules" "${D}/pkg/main/${PKG}.core.${PVR}/lib$LIB_SUFFIX/xorg/modules"
 
 finalize
