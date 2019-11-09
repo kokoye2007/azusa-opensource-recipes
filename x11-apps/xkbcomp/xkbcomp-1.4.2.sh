a38e80bf95b80a306d0c746e6f71c6918234687a
Mark Karpeles <magicaltux@gmail.com>
2019-11-09 17:29:46 +0900

    xkbcomp: add symlink to xkeyboard-config so X will work

--- x11-apps/xkbcomp/xkbcomp-1.4.2.sh
+++ x11-apps/xkbcomp/xkbcomp-1.4.2.sh
@@ -2,12 +2,16 @@
 source "../../common/init.sh"
 
 get https://www.x.org/pub/individual/app/${P}.tar.bz2
+acheck
 
 cd "${T}"
 
-doconf --localstatedir=/var
+doconf
 
 make
 make install DESTDIR="${D}"
+
+# create a symlink to /pkg/main/x11-misc.xkeyboard-config.core/share since somehow X11 will look for xkb rules here
+ln -snfTv /pkg/main/x11-misc.xkeyboard-config.core/share "${D}/pkg/main/${PKG}.core.${PVR}/share"
 
 finalize
