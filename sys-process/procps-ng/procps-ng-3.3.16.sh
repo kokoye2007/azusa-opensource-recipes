dfde477378261c94825b32ef33f6e29a5331f414
Mark Karpeles <magicaltux@gmail.com>
2020-02-09 21:30:38 +0900

    bump procps-ng-3.3.16

--- sys-process/procps-ng/procps-ng-3.3.16.sh
+++ sys-process/procps-ng/procps-ng-3.3.16.sh
@@ -2,8 +2,11 @@
 source "../../common/init.sh"
 
 get https://sourceforge.net/projects/${PN}/files/Production/${P}.tar.xz
+acheck
 
 cd "${P}"
+
+importpkg ncurses
 
 doconf --disable-static --disable-kill
 
