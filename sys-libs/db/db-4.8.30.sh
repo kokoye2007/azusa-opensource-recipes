eb869b5619d2bf83a117fc2522adb518798c158c
Mark Karpeles <magicaltux@gmail.com>
2020-02-26 08:36:39 +0900

    add db-4.8.30

---
+++ sys-libs/db/db-4.8.30.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get http://download.oracle.com/berkeley-db/${P}.tar.gz
+acheck
+
+cd "${P}"
+
+sed -i 's/\(__atomic_compare_exchange\)/\1_db/' dbinc/atomic.h
+
+cd "${T}"
+
+CONFPATH="${CHPATH}/${P}/dist/configure" doconf --enable-compat185 --enable-dbm --disable-static --enable-cxx
+
+make
+make install DESTDIR="${D}"
+
+finalize
