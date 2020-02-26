8deea5d165ef4b0e8b8903f4ec87d5ca744a2a78
Mark Karpeles <magicaltux@gmail.com>
2020-02-26 12:34:34 +0900

    add sudo

---
+++ app-admin/sudo/sudo-1.8.31.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://www.sudo.ws/dist/${P}.tar.gz
+acheck
+
+cd "${T}"
+
+importpkg zlib
+
+doconf --enable-zlib=system --enable-tmpfiles.d=/usr/lib/tmpfiles.d --with-editor=/usr/libexec/editor --with-env-editor --with-plugindir=/pkg/main/${PKG}.libs.${PVR}/sudo --with-rundir=/run/sudo --with-vardir=/var/db/sudo --without-linux-audit --without-opie --with-pam
+
+make
+make install DESTDIR="${D}"
+
+finalize
