0cdbcaf1696d6cf5044dda70346eeedf4483515b
Mark Karpeles <magicaltux@gmail.com>
2020-02-27 15:41:43 +0900

    dbus-python-1.2.14

---
+++ dev-python/dbus-python/dbus-python-1.2.14.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+source ../../common/init.sh
+source ${ROOTDIR}/common/python.sh
+
+get https://dbus.freedesktop.org/releases/${PN}/${P}.tar.gz
+acheck
+
+cd "${P}"
+
+pythonsetup
+archive
