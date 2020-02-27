34026b3e59e0aabf5029cf17729722ce2d593500
Mark Karpeles <magicaltux@gmail.com>
2020-02-27 22:04:09 +0900

    fix imagick6 too

--- media-gfx/imagemagick/imagemagick-6.9.10-96.sh
+++ media-gfx/imagemagick/imagemagick-6.9.10-96.sh
@@ -10,9 +10,12 @@ patch -p1 <"$FILESDIR/ImageMagick-6.9.10-60-libs_only-1.patch"
 
 cd "${T}"
 
-importpkg sys-devel/libtool app-arch/bzip2 sys-apps/util-linux
+importpkg sys-devel/libtool app-arch/bzip2 sys-apps/util-linux dev-libs/libbsd
 
 doconf --sysconfdir=/etc --enable-hdri --with-modules --disable-static
+
+# required during build
+ln -s /pkg/main/sys-devel.libtool.libs/lib64/libltdl.la /usr/lib64/libltdl.la
 
 make
 make install DESTDIR="${D}"
