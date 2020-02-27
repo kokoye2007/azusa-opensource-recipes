679f2c0cf47cace3a329f47458c5c162efbf11d5
Mark Karpeles <magicaltux@gmail.com>
2020-02-28 08:21:16 +0900

    fix build of gd

--- media-libs/gd/gd-2.2.5.sh
+++ media-libs/gd/gd-2.2.5.sh
@@ -2,18 +2,23 @@
 source "../../common/init.sh"
 
 get https://github.com/libgd/libgd/releases/download/${P}/lib${P}.tar.xz
+acheck
 
 cd "lib${P}"
 
-patch -p1 <"$FILESDIR/${P}-ossfuzz5700.patch"
-patch -p1 <"$FILESDIR/${P}-CVE-2018-5711.patch"
-patch -p1 <"$FILESDIR/${P}-CVE-2018-1000222.patch"
-patch -p1 <"$FILESDIR/${P}-CVE-2019-6977.patch"
-patch -p1 <"$FILESDIR/${P}-CVE-2019-6978.patch"
+PATCHES=(
+	"$FILESDIR/${P}-ossfuzz5700.patch"
+	"$FILESDIR/${P}-CVE-2018-5711.patch"
+	"$FILESDIR/${P}-CVE-2018-1000222.patch"
+	"$FILESDIR/${P}-CVE-2019-6977.patch"
+	"$FILESDIR/${P}-CVE-2019-6978.patch"
+)
+apatch $PATCHES
+aautoreconf
 
 cd "${T}"
 
-importpkg libjpeg libwebp
+importpkg libjpeg libwebp uuid app-arch/bzip2 liblzma libbsd
 
 doconf --disable-static --disable-werror --without-x --without-liq --with-zlib --with-png --with-freetype --with-fontconfig --with-jpeg=/pkg/main/media-libs.libjpeg-turbo.dev --with-xpm --with-tiff --with-webp
 
