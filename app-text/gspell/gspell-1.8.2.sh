ea86eef541633ff2af25d39c26c5ff721b4ebac7
Mark Karpeles <magicaltux@gmail.com>
2020-02-28 15:28:42 +0900

    fix gspell, bump vala & rust

--- app-text/gspell/gspell-1.8.2.sh
+++ app-text/gspell/gspell-1.8.2.sh
@@ -2,8 +2,11 @@
 source "../../common/init.sh"
 
 get https://ftp.gnome.org/pub/gnome/sources/gspell/1.8/${P}.tar.xz
+acheck
 
 cd "${T}"
+
+importpkg uuid app-arch/bzip2 libbsd app-text/aspell
 
 doconf
 
