a1f34d34ef3aa73e4fbb59592b4fecd3b2933d89
Mark Karpeles <magicaltux@gmail.com>
2020-02-29 02:55:28 +0900

    add botan-2.13.0

---
+++ dev-libs/botan/botan-2.13.0.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://botan.randombit.net/releases/Botan-${PV}.tar.xz
+acheck
+
+cd "Botan-${PV}"
+
+importpkg openssl zlib app-arch/bzip2 sqlite3 liblzma
+
+python3 configure.py \
+	--extra-cxxflags="${CPPFLAGS}" --ldflags="${LDFLAGS}" \
+	--system-cert-bundle=/etc/ssl/certs/ca-certificates.crt --with-openmp --with-python-versions=3.8 --with-boost --with-bzip2 --with-lzma --with-openssl --with-commoncrypto --with-sqlite3 --with-tpm --with-zlib --prefix=/pkg/main/${PKG}.core.${PVR} --libdir=/pkg/main/${PKG}.libs.${PVR}/lib$LIB_SUFFIX --includedir=/pkg/main/${PKG}.dev.${PVR}/include
+
+make
+make install DESTDIR="${D}"
+
+finalize
