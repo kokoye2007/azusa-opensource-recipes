9f3f708d3a49c7d3b40b20b10ed27712c522fd3b
Mark Karpeles <magicaltux@gmail.com>
2020-02-28 15:47:14 +0900

    bump glibc-2.31

--- sys-libs/glibc/glibc-2.31.sh
+++ sys-libs/glibc/glibc-2.31.sh
@@ -3,11 +3,12 @@ source "../../common/init.sh"
 
 # fetch xz, compile, build
 get http://ftp.jaist.ac.jp/pub/GNU/libc/${P}.tar.xz
+acheck
 
 cd "${T}"
 
 # configure & build
-doconf --disable-werror --enable-kernel=3.2 --enable-stack-protector=strong --with-headers=/usr/include libc_cv_slibdir=/pkg/main/${PKG}.libs.${PVR}/lib$LIB_SUFFIX
+doconf --disable-werror --enable-kernel=4.19 --enable-stack-protector=strong --with-headers=/usr/include libc_cv_slibdir=/pkg/main/${PKG}.libs.${PVR}/lib$LIB_SUFFIX
 
 make
 make install DESTDIR="${D}"
