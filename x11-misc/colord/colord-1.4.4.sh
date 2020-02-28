29a44e9b73853cff24b12b784c0ef0d4c3e46f84
Mark Karpeles <magicaltux@gmail.com>
2020-02-29 01:57:22 +0900

    add colord

---
+++ x11-misc/colord/colord-1.4.4.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://www.freedesktop.org/software/colord/releases/${P}.tar.xz
+acheck
+
+cd "${T}"
+
+# TODO fix man building (xsltproc fails)
+meson --prefix="/pkg/main/${PKG}.core.${PVR}" "${CHPATH}/${P}" -Dman=false
+
+ninja
+DESTDIR="${D}" ninja install
+
+finalize
