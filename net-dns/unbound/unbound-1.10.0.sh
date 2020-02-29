b9289071760145aac153b04bcfeb8a07ee2e0c52
Mark Karpeles <magicaltux@gmail.com>
2020-02-29 23:55:26 +0900

    upgrade unbound

---
+++ net-dns/unbound/unbound-1.10.0.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get http://www.unbound.net/downloads/${P}.tar.gz
+acheck
+
+cd "${T}"
+
+doconf --sysconfdir=/etc --disable-static --with-pidfile=/var/run/unbound.pid --with-ssl=/pkg/main/dev-libs.openssl.dev --with-libexpat=/pkg/main/dev-libs.expat.dev
+
+make
+make install DESTDIR="${D}"
+
+finalize
