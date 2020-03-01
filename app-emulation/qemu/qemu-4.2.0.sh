961b0b8823128b3f6af4c87b77eed33e9cbec4e4
Mark Karpeles <magicaltux@gmail.com>
2020-03-01 14:05:10 +0900

    cairo bump

--- app-emulation/qemu/qemu-4.2.0.sh
+++ app-emulation/qemu/qemu-4.2.0.sh
@@ -12,7 +12,8 @@ importpkg media-libs/alsa-lib sys-fs/udev
 export CFLAGS="$CPPFLAGS"
 
 callconf --prefix="/pkg/main/${PKG}.core.${PVR}" --interp-prefix="/pkg/main/${PKG}.mod.${PVR}.%M" --mandir="/pkg/main/${PKG}.doc.${PVR}/man" --docdir="/pkg/main/${PKG}.doc.${PVR}" --libdir=lib$LIB_SUFFIX --sysconfdir=/etc \
-	--audio-drv-list=alsa --enable-malloc-trim
+	--audio-drv-list=alsa --enable-malloc-trim \
+	--enable-sdl --enable-sdl-image --enable-xkbcommon --enable-gtk --enable-vte --enable-opengl --enable-virglrenderer
 
 make -j8
 make install DESTDIR="${D}"
---
+++ x11-libs/vte/vte-0.58.3.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get http://ftp.nara.wide.ad.jp/pub/X11/GNOME/sources/vte/${PV:0:4}/${P}.tar.xz
+acheck
+
+cd "${T}"
+
+# TODO fix man building (xsltproc fails)
+meson --prefix="/pkg/main/${PKG}.core.${PVR}" "${CHPATH}/${P}"
+
+ninja
+DESTDIR="${D}" ninja install
+
+finalize
