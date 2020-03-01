9f69eb28179d3212a17b33fa34e1ba17b4cbf3f6
Mark Karpeles <magicaltux@gmail.com>
2020-03-01 16:55:13 +0900

    cairo check too logn

--- x11-libs/cairo/cairo-1.16.0.sh
+++ x11-libs/cairo/cairo-1.16.0.sh
@@ -11,7 +11,6 @@ importpkg zlib
 doconf --disable-static -with-x --enable-tee --enable-xlib --enable-xlib-xrender --enable-xcb --enable-xcb-shm --enable-gobject --enable-gl --enable-svg --enable-ft --enable-interpreter --enable-pdf --enable-png --enable-ps --enable-script --disable-drm --disable-directfb --disable-gallium --disable-qt --disable-vg --disable-xlib-xcb --enable-full-testing
 
 make
-make check
 make install DESTDIR="${D}"
 
 finalize
