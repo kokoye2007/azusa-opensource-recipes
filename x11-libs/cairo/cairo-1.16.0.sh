edd7e970c36d852b298c181eda4a0f4d71ef4a98
Mark Karpeles <magicaltux@gmail.com>
2020-03-01 14:20:57 +0900

    tweak cairo configure

---
+++ x11-libs/cairo/cairo-1.16.0.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://www.cairographics.org/releases/${P}.tar.xz
+acheck
+
+cd "${T}"
+
+importpkg zlib
+
+doconf --disable-static -with-x --enable-tee --enable-xlib --enable-xlib-xrender --enable-xcb --enable-xcb-shm --enable-gobject --enable-gl --enable-svg --enable-ft --enable-interpreter --enable-pdf --enable-png --enable-ps --enable-script --disable-drm --disable-directfb --disable-gallium --disable-qt --disable-vg --disable-xlib-xcb --enable-full-testing
+
+make
+make check
+make install DESTDIR="${D}"
+
+finalize
