dc9f7d6f0323c9c57a9373bcf7bd8e05ffc7d663
Mark Karpeles <magicaltux@gmail.com>
2020-03-02 19:18:21 +0900

    add deps for e2fsprogs

--- sys-fs/e2fsprogs/e2fsprogs-1.45.0.sh
+++ sys-fs/e2fsprogs/e2fsprogs-1.45.0.sh
@@ -2,8 +2,11 @@
 source "../../common/init.sh"
 
 get https://sourceforge.net/projects/${PN}/files/${PN}/v${PV}/${P}.tar.gz
+acheck
 
 cd "${T}"
+
+importpkg uuid sys-apps/util-linux
 
 doconf --enable-elf-shlibs --disable-libblkid --disable-libuuid --disable-uuidd --disable-fsck
 
