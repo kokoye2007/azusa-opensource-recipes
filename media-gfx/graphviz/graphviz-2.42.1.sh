0955f8aa39d5850026fa4c5dc90dc78219eee5f2
Mark Karpeles <magicaltux@gmail.com>
2020-03-03 02:08:41 +0900

    fix graphviz

--- media-gfx/graphviz/graphviz-2.42.1.sh
+++ media-gfx/graphviz/graphviz-2.42.1.sh
@@ -2,18 +2,27 @@
 source "../../common/init.sh"
 
 get https://www2.graphviz.org/Packages/stable/portable_source/${P}.tar.gz
+acheck
 
 cd "${P}"
 
 sed -i '/LIBPOSTFIX="64"/s/64//' configure.ac
 
-autoreconf
+aautoreconf
 
 cd "${T}"
 
-doconf
+doconf --with-webp=yes --with-poppler=yes --with-rsvg=yes --with-pangocairo=yes --with-freetype2=yes --with-fontconfig=yes
 
 make
 make install DESTDIR="${D}"
+
+# run dot -c to create  /.pkg-main-rw/media-gfx.graphviz.libs.2.42.1.linux.amd64/lib64/graphviz/config6
+make install DESTDIR="/"
+
+echo "Running dot -c"
+"${D}/pkg/main/${PKG}.core.${PVR}/bin/dot" -c
+mkdir -p "${D}/${PKG}.libs.${PVR}/lib$LIB_SUFFIX/graphviz/"
+mv -v "/.pkg-main-rw/${PKG}.libs.${PVR}/lib$LIB_SUFFIX/graphviz/config"* "${D}/${PKG}.libs.${PVR}/lib$LIB_SUFFIX/graphviz/"
 
 finalize
