4845d834e95b422648fbe357ddc8667d8231072e
Mark Karpeles <magicaltux@gmail.com>
2020-03-03 00:52:43 +0900

    fix build for z3 & kbd

--- sci-mathematics/z3/z3-4.8.7.sh
+++ sci-mathematics/z3/z3-4.8.7.sh
@@ -11,6 +11,10 @@ cd */
 doconflight --gmp
 
 cd build
+
+# config.mk is missing -L flag for libgmp in SLINK_EXTRA_FLAGS=
+sed -i -e "s,^SLINK_EXTRA_FLAGS=,SLINK_EXTRA_FLAGS=$(pkg-config --libs-only-L gmp) ," config.mk
+
 make -j6
 make install DESTDIR="${D}"
 
--- sys-apps/kbd/kbd-2.0.4.sh
+++ sys-apps/kbd/kbd-2.0.4.sh
@@ -2,8 +2,11 @@
 source "../../common/init.sh"
 
 get https://mirrors.edge.kernel.org/pub/linux/utils/kbd/${P}.tar.xz
+acheck
 
 cd "${T}"
+
+importpkg sys-libs/pam dev-libs/check
 
 doconf
 
