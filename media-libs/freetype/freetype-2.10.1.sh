6f5b2e85095896c730888be6858a3ac9e710523f
Mark Karpeles <magicaltux@gmail.com>
2020-03-02 18:22:50 +0900

    freetype: add bzip2 pkg import

--- media-libs/freetype/freetype-2.10.1.sh
+++ media-libs/freetype/freetype-2.10.1.sh
@@ -2,6 +2,7 @@
 source "../../common/init.sh"
 
 get https://download.savannah.gnu.org/releases/freetype/${P}.tar.xz
+acheck
 
 cd "${P}"
 
@@ -9,6 +10,8 @@ sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg
 sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" -i include/freetype/config/ftoption.h
 
 cd "${T}"
+
+importpkg app-arch/bzip2
 
 # force harfbuzz to yes so we get an error if it is not detected
 doconf --enable-freetype-config --disable-static --with-zlib=yes --with-bzip2=yes --with-png=yes --with-harfbuzz=yes
