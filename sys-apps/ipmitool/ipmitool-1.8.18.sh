a0a7ff51fefb40aa8d11176cca85fb5fab91cd03
Mark Karpeles <magicaltux@gmail.com>
2020-03-05 00:11:29 +0900

    add ipmitool

--- sys-apps/ipmitool/ipmitool-1.8.18.sh
+++ sys-apps/ipmitool/ipmitool-1.8.18.sh
@@ -4,9 +4,13 @@ source "../../common/init.sh"
 get https://github.com/ipmitool/ipmitool/releases/download/IPMITOOL_1_8_18/${P}.tar.bz2
 acheck
 
-importpkg tinfo sys-libs/readline openssl
+cd "${P}"
+
+apatch "${FILESDIR}/ipmitool-1.8.18-openssl-1.1.patch"
 
 cd "${T}"
+
+importpkg tinfo sys-libs/readline openssl sys-libs/freeipmi
 
 doconf --enable-intf-lan --enable-intf-usb --enable-intf-lanplus --enable-intf-serial --with-kerneldir=/pkg/main/sys-kernel.linux.dev --enable-intf-free --enable-intf-open --enable-intf-imb --enable-ipmishell --enable-file-security
 
