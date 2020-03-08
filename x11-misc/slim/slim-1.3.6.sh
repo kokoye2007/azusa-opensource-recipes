b94153384b4148b1e2b168e6fc2b2e4f0532224d
Mark Karpeles <magicaltux@gmail.com>
2020-03-08 11:17:46 +0900

    attempt at getting slim to compile, added consolekit

--- x11-misc/slim/slim-1.3.6.sh
+++ x11-misc/slim/slim-1.3.6.sh
@@ -25,20 +25,24 @@ apatch "${PATCHES[@]}"
 
 cd "${T}"
 
+importpkg libpng libjpeg zlib sys-libs/pam x11-base/xorg-proto
+
+export CXXFLAGS="${CPPFLAGS} -O2"
+
 CMAKEOPTS=(
 	-DUSE_PAM=ON
 	-DUSE_CONSOLEKIT=ON
-	-DX11_X11_INCLUDE_PATH=/pkg/main/x11-libs.libX11.dev/include
-	-DX11_X11_LIB=/pkg/main/x11-libs.libX11.libs/lib$LIB_SUFFIX
 	-DFREETYPE_INCLUDE_DIRS=/pkg/main/media-libs.freetype.dev/include
 	-DFREETYPE_LIBRARY=/pkg/main/media-libs.freetype.libs/lib$LIB_SUFFIX
-	-DJPEG_INCLUDE_DIR=/pkg/main/media-libs.libjpeg-turbo.dev/include
-	-DJPEG_LIBRARY=/pkg/main/media-libs.libjpeg-turbo.libs/lib$LIB_SUFFIX
-	-DZLIB_INCLUDE_DIR=/pkg/main/sys-libs.zlib.dev/include
-	-DZLIB_LIBRARY=/pkg/main/sys-libs.zlib.libs/lib$LIB_SUFFIX
-	-DPNG_PNG_INCLUDE_DIR=/pkg/main/media-libs.libpng.dev/include
-	-DPNG_LIBRARY=/pkg/main/media-libs.libpng.libs/lib$LIB_SUFFIX
+	-DCRYPTO_LIB=/pkg/main/dev-libs.openssl.libs/lib$LIB_SUFFIX/libcrypto.so
+	-DM_LIB=/pkg/main/sys-libs.glibc.libs/lib64/libm.so
+	-DRT_LIB=/pkg/main/sys-libs.glibc.libs/lib64/librt.so
 )
+
+for foo in X11 Xext Xrender Xrandr Xmu Xft; do
+	CMAKEOPTS+=("-DX11_${foo}_LIB=/pkg/main/x11-libs.lib${foo}.libs/lib$LIB_SUFFIX/lib${foo}.so")
+	CMAKEOPTS+=("-DX11_${foo}_INCLUDE_PATH=/pkg/main/x11-libs.lib${foo}.dev/include")
+done
 
 docmake "${CMAKEOPTS[@]}"
 
