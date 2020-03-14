65a121282c718471cc5ff7985ea07ceb7face332
Mark Karpeles <magicaltux@gmail.com>
2020-03-15 04:31:46 +0900

    gegl now compiles but cogl wont

---
+++ media-libs/cogl/cogl-1.22.6.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get http://ftp.gnome.org/pub/gnome/sources/cogl/1.22/${P}.tar.xz
+acheck
+
+cd "${T}"
+
+importpkg dev-libs/wayland media-libs/mesa
+
+export CFLAGS="${CPPFLAGS} -O2"
+
+doconf --disable-examples-install --disable-maintainer-flags --enable-cairo --enable-gdk-pixbuf --enable-glib --enable-introspection --enable-gles1 --enable-gles2 --enable-{kms,wayland,xlib}-egl-platform --enable-wayland-egl-server
+
+make -j"$NPROC"
+make install DESTDIR="${D}"
+
+finalize
--- media-libs/gegl/gegl-0.4.16.sh
+++ media-libs/gegl/gegl-0.4.16.sh
@@ -8,14 +8,17 @@ cd "${T}"
 
 importpkg libjpeg
 
+# make asciidoc work
+ln -snfT /pkg/main/app-text.asciidoc.core/etc/asciidoc /etc/asciidoc
+
 doconf
 
 make
 make install DESTDIR="${D}"
 
-mkdir -p "${D}/pkg/main/${PKG}.doc.${PVR}/html/images"
+#mkdir -p "${D}/pkg/main/${PKG}.doc.${PVR}/html/images"
 
-install -v -m644 docs/*.{css,html} "${D}/pkg/main/${PKG}.doc.${PVR}/html"
-install -v -m644 docs/images/*.{png,ico,svg} "${D}/pkg/main/${PKG}.doc.${PVR}/html/images"
+#install -v -m644 docs/*.{css,html} "${D}/pkg/main/${PKG}.doc.${PVR}/html"
+#install -v -m644 docs/images/*.{png,ico,svg} "${D}/pkg/main/${PKG}.doc.${PVR}/html/images"
 
 finalize
