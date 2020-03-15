b1ec481fe89c4e5ee9a2425401810090e246ecfb
Mark Karpeles <magicaltux@gmail.com>
2020-03-16 03:17:05 +0900

    fix libdrm

--- x11-libs/libdrm/libdrm-2.4.100.sh
+++ x11-libs/libdrm/libdrm-2.4.100.sh
@@ -8,6 +8,6 @@ cd "${T}"
 
 importpkg app-arch/bzip2 sys-apps/util-linux dev-libs/libbsd
 
-domeson -Dudev=false -Dcairo-tests=false -Damdgpu=true -Dexynos=false -Dfreedreno=false -Dintel=true -Dnouveau=true -Domap=false -Dradeon=true -Dtegra=false -Dvc4=false -Detnaviv=false -Dvmwgfx=false -Dlibkms=true -Dvalgrind=auto
+domeson -Dudev=true -Dcairo-tests=false -Damdgpu=true -Dexynos=true -Dfreedreno=true -Dintel=true -Dnouveau=true -Domap=true -Dradeon=true -Dtegra=true -Dvc4=true -Detnaviv=true -Dvmwgfx=true -Dlibkms=true -Dvalgrind=auto
 
 finalize
