b746f90bff0929c88e13cea69e1a02db46f91bd9
Mark Karpeles <magicaltux@gmail.com>
2020-03-16 10:57:42 +0900

    eudev build

--- sys-fs/eudev/eudev-3.2.9.sh
+++ sys-fs/eudev/eudev-3.2.9.sh
@@ -2,10 +2,13 @@
 source "../../common/init.sh"
 
 get https://dev.gentoo.org/~blueness/eudev/${P}.tar.gz
+acheck
 
 cd "${T}"
 
-doconf --enable-manpages --disable-static --config-cache
+importpkg sys-apps/util-linux
+
+doconf --enable-manpages --disable-static --config-cache --enable-hwdb --with-rootlibexecdir=/lib$LIB_SUFFIX/udev
 
 make
 make install DESTDIR="${D}"
