4d50a494e78d6ff46f7a91150a668fe1f95a2926
Mark Karpeles <magicaltux@gmail.com>
2020-03-18 22:05:33 +0900

    adding libproxy and hexchat

---
+++ net-irc/hexchat/hexchat-2.14.3.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://dl.hexchat.net/hexchat/${P}.tar.xz
+acheck
+
+cd "${T}"
+
+domeson
+
+finalize
---
+++ net-libs/libproxy/libproxy-0.4.15.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/${PN}/${PN}/releases/download/${PV}/${P}.tar.xz
+acheck
+
+cd "${T}"
+
+importpkg zlib
+
+docmake -DWITH_NATUS=OFF -DWITH_PYTHON2=OFF -DWITH_VALA=ON -DWITH_WEBKIT=OFF -DWITH_PERL=OFF -DWITH_PYTHON3=OFF
+# -DFORCE_SYSTEM_LIBMODMAN=ON
+
+make
+make install DESTDIR="${D}"
+
+finalize
