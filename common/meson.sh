a5f85ea24f44a71a80ddb43c30e77c5882bff794
Mark Karpeles <magicaltux@gmail.com>
2020-03-19 03:30:14 +0900

    move cmake and meson methods to own files

---
+++ common/meson.sh
@@ -0,0 +1,15 @@
+# Meson build methods
+
+domeson() {
+	echo "Running meson..."
+	if [ x"$MESON_ROOT" = x ]; then
+		MESON_ROOT="${S}"
+	fi
+
+	meson "$MESON_ROOT" --prefix="/pkg/main/${PKG}.core.${PVR}" -Dbuildtype=release "$@" || return $?
+
+	echo "Building with ninja..."
+	ninja || return $?
+	DESTDIR="${D}" ninja install || return $?
+}
+
--- media-libs/opencv/opencv-3.4.7.sh
+++ media-libs/opencv/opencv-3.4.7.sh
@@ -2,10 +2,9 @@
 source "../../common/init.sh"
 
 get https://github.com/opencv/${PN}/archive/${PV}.tar.gz
+acheck
 
 cd "${T}"
-
-importcmakepkg sys-libs/zlib
 
 docmake -DBUILD_SHARED_LIBS=ON -DOPENCV_GENERATE_PKGCONFIG=YES
 
