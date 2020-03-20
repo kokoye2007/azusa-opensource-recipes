ac756e9f7cff9d09b8d201c58384a4df32829839
Mark Karpeles <magicaltux@gmail.com>
2020-03-21 06:50:32 +0900

    fix clutter build

--- media-libs/clutter/clutter-1.26.2.sh
+++ media-libs/clutter/clutter-1.26.2.sh
@@ -3,6 +3,8 @@ source "../../common/init.sh"
 
 get http://ftp.gnome.org/pub/gnome/sources/clutter/1.26/${P}.tar.xz
 
+importpkg zlib
+
 cd "${T}"
 
 doconf --sysconfdir=/etc --enable-egl-backend --enable-evdev-input --enable-wayland-backend --enable-wayland-compositor
