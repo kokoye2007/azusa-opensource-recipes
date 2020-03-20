c7a9119df03ad3b8457da7c7b3057f7ae7de1257
Mark Karpeles <magicaltux@gmail.com>
2020-03-21 01:23:50 +0900

    adding glew, mesa-progs

---
+++ x11-apps/mesa-progs/mesa-progs-8.4.0.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+MY_PN="${PN/progs/demos}"
+MY_P="${MY_PN}-${PV}"
+
+get https://mesa.freedesktop.org/archive/demos/${MY_P}.tar.bz2
+acheck
+
+cd "${T}"
+
+importpkg media-libs/glu media-libs/freeglut
+
+doconf
+
+make
+make install DESTDIR="${D}"
+
+finalize
