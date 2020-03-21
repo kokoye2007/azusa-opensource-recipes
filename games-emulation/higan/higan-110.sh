cf68ac1b1b644b3ac37faff405405f047f95f5b8
Mark Karpeles <magicaltux@gmail.com>
2020-03-22 04:49:35 +0900

    higan now builds

--- games-emulation/higan/higan-110.sh
+++ games-emulation/higan/higan-110.sh
@@ -4,15 +4,27 @@ source "../../common/init.sh"
 get https://github.com/byuu/higan/archive/v${PV}.tar.gz ${P}.tar.gz
 acheck
 
+cd "${S}"
+
+apatch "$FILESDIR/${P}-make-install.patch"
+
 cd "${S}/higan"
 
-importpkg X media-libs/mesa media-libs/alsa-lib media-libs/libao media-libs/openal media-sound/pulseaudio media-libs/libsdl2 libudev
+importpkg X media-libs/mesa media-libs/alsa-lib media-libs/libao media-libs/openal media-sound/pulseaudio media-libs/libsdl2 libudev zlib
 
 # provide flags & options ENV vars for build process
 export flags="${CPPFLAGS} -O2"
 export options="${LDFLAGS}"
+export hiro=gtk3
+export platform=linux
+export prefix="/pkg/main/${PKG}.core.${PVR}"
 
-make platform="linux" hiro=gtk3
-make install DESTDIR="${D}"
+make -j"$NPROC"
+make install prefix="${D}$prefix"
+
+# move systems
+mkdir -p "${D}/pkg/main/${PKG}.data.${PVR}"
+mv -Tv "${D}$prefix/share/higan" "${D}/pkg/main/${PKG}.data.${PVR}/higan"
+ln -snfTv "/pkg/main/${PKG}.data.${PVR}/higan" "${D}$prefix/share/higan"
 
 finalize
