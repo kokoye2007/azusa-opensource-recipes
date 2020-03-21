0b0e37a57dd1f52676db23b1bc72d8f25cbe861b
Mark Karpeles <magicaltux@gmail.com>
2020-03-22 04:32:15 +0900

    fix higan build

--- games-emulation/higan/higan-110.sh
+++ games-emulation/higan/higan-110.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 source "../../common/init.sh"
 
-get https://github.com/byuu/higan/archive/v${PV}.tar.gz
+get https://github.com/byuu/higan/archive/v${PV}.tar.gz ${P}.tar.gz
 acheck
 
-cd "${P}"
-cd higan
+cd "${S}/higan"
 
-make platform="linux" hiro=gtk
+importpkg X media-libs/mesa media-libs/alsa-lib media-libs/libao media-libs/openal media-sound/pulseaudio media-libs/libsdl2 libudev
+
+# provide flags & options ENV vars for build process
+export flags="${CPPFLAGS} -O2"
+export options="${LDFLAGS}"
+
+make platform="linux" hiro=gtk3
 make install DESTDIR="${D}"
 
 finalize
