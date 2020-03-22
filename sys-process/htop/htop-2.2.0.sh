ce7cbcf9e50f942469f715600caebd1dcb08d15b
Mark Karpeles <magicaltux@gmail.com>
2020-03-23 07:57:55 +0900

    fix htop

--- sys-process/htop/htop-2.2.0.sh
+++ sys-process/htop/htop-2.2.0.sh
@@ -4,9 +4,15 @@ source "../../common/init.sh"
 get https://hisham.hm/htop/releases/${PV}/${P}.tar.gz
 acheck
 
+cd "${S}"
+
+rm -v missing
+
 cd "${T}"
 
 importpkg sys-libs/ncurses
+
+export CPPFLAGS="${CPPFLAGS} -DMAJOR_IN_SYSMACROS"
 
 doconf --disable-hwloc --enable-taskstats --enable-cgroup --enable-linux-affinity --enable-unicode
 
