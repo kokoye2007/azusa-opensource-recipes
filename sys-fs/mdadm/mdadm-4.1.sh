3d5386caf407408a05259d9c59a566b95e1fa30c
Mark Karpeles <magicaltux@gmail.com>
2020-08-03 02:56:17 +0900

    add mdadm

---
+++ sys-fs/mdadm/mdadm-4.1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://www.kernel.org/pub/linux/utils/raid/mdadm/${P}.tar.xz
+acheck
+
+cd "${P}"
+
+sed 's@-Werror@@' -i Makefile
+
+make
+make install DESTDIR="${D}" BINDIR="/pkg/main/${PKG}.core.${PVR}/sbin" MANDIR="/pkg/main/${PKG}.doc.${PVR}/man"
+
+# install static mdadm in dev pkg (used for initramfs, etc)
+make mdadm.static
+/usr/bin/install -D -m 755 mdadm.static "${D}/pkg/main/${PKG}.dev.${PVR}/sbin/mdadm.static"
+
+finalize
