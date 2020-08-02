8a58a365b89bafd33331b8df014018f2636a66c2
Mark Karpeles <magicaltux@gmail.com>
2020-08-03 03:15:42 +0900

    added lvm2 static

---
+++ sys-fs/lvm2-static/lvm2-static-2.03.09.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get http://mirrors.kernel.org/sourceware/lvm2/LVM2.${PV}.tgz
+acheck
+
+cd "${T}"
+
+importpkg dev-libs/libaio
+export CFLAGS="$CPPFLAGS"
+
+doconf --enable-static_link --disable-readline --disable-use-lvmlockd --disable-use-lvmpolld --disable-udev_sync --disable-udev_rules --disable-dbus-service --disable-selinux
+
+make STATIC_LIBS="-lpthread -lm" interfacebuilddir="$PWD/libdm/ioctl" || /bin/bash -i
+make install DESTDIR="${D}"
+
+finalize
