7500e9d45a231e0e00a6840efbc430424dcac889
Mark Karpeles <magicaltux@gmail.com>
2020-08-09 23:12:28 +0900

    static parted (wasnt easy to achieve)

---
+++ sys-block/parted-static/parted-static-3.3.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://ftp.gnu.org/gnu/parted/parted-${PV}.tar.xz
+acheck
+
+cd "${T}"
+
+importpkg sys-apps/util-linux sys-fs/lvm2 sys-libs/readline sys-libs/ncurses
+
+export CFLAGS="-static -pthread"
+export LDFLAGS="-static -pthread -L/pkg/main/sys-fs.lvm2-static.libs/lib64 -L/pkg/main/sys-apps.util-linux.libs/lib64"
+export LIBS="-lblkid -lm" # static libs
+
+# TODO give path of the static libdevmapper
+# /pkg/main/sys-fs.lvm2-static.libs/lib64
+doconf --enable-all-static --without-readline --disable-nls --disable-shared --disable-dynamic-loading
+
+# fix calls to major() and minor()
+echo "#include <sys/sysmacros.h>" >>lib/config.h
+
+make V=1
+
+# parted is unable to correctly build itself statically, so let's do it
+showrun() {
+	echo "Running: $*"
+	"$@"
+}
+
+TARGET="${D}/pkg/main/${PKG}.core.${PVR}/sbin"
+mkdir -p "$TARGET"
+
+cd parted
+showrun gcc $LDFLAGS -Wl,--as-needed -o parted.static command.o parted.o strlist.o ui.o table.o  libver.a ../libparted/.libs/libparted.a -ldevmapper -luuid $LIBS
+cp -v parted.static "$TARGET"
+cd ../partprobe
+showrun gcc $LDFLAGS -o partprobe.static partprobe.o ../libparted/.libs/libparted.a -ldevmapper -luuid $LIBS
+cp -v partprobe.static "$TARGET"
+
+finalize
