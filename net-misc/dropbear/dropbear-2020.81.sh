3eac4d1e6f54132b3f8b62bab8e261bab9da147d
Mark Karpeles <magicaltux@gmail.com>
2021-02-22 13:55:16 +0900

    add dropbear as static binary

---
+++ net-misc/dropbear/dropbear-2020.81.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://matt.ucc.asn.au/dropbear/releases/${P}.tar.bz2
+acheck
+
+cd "${T}"
+
+doconf --enable-static --enable-bundled-libtom --disable-zlib
+
+make
+make install DESTDIR="${D}"
+
+finalize
