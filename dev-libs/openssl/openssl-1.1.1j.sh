8279f88e580c8dd597f44085c8c2a374b936e1fd
Mark Karpeles <magicaltux@gmail.com>
2021-03-11 18:22:42 +0900

    remove old openssl, fix build

--- dev-libs/openssl/openssl-1.1.1j.sh
+++ dev-libs/openssl/openssl-1.1.1j.sh
@@ -2,8 +2,11 @@
 source "../../common/init.sh"
 
 get https://www.openssl.org/source/${P}.tar.gz
+acheck
 
 cd "${P}"
+
+importpkg zlib
 
 ./config --prefix=/pkg/main/${PKG}.core.${PVRF} --openssldir=/etc/ssl --libdir=/pkg/main/${PKG}.libs.${PVRF}/lib$LIB_SUFFIX shared zlib-dynamic
 
