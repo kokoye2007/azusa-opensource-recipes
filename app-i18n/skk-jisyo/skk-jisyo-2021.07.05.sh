0c91ca05726cf5c9011d5a8a55f5fb6f73e3f961
Mark Karpeles <magicaltux@gmail.com>
2021-08-01 16:26:35 +0900

    simple add skk-jisyo

---
+++ app-i18n/skk-jisyo/skk-jisyo-2021.07.05.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+git clone --no-checkout https://github.com/skk-dev/dict.git 
+cd dict
+git checkout -q "master@{${PV//./-}}"
+acheck
+
+# those are EUC-JP format
+mkdir -p "${D}/pkg/main/${PKG}.core.${PVRF}/skk"
+cp SKK-JISYO.* "${D}/pkg/main/${PKG}.core.${PVRF}/skk"
+
+finalize
