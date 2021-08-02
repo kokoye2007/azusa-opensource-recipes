8069a4a1c0738c19b5b84cd398281c087ea6a914
Mark Karpeles <magicaltux@gmail.com>
2021-08-03 02:48:53 +0900

    bumping stuff, adding gcc-config package for gcc config

---
+++ sys-devel/gcc-config/gcc-config-0.0.1.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+TZ=`date +%Y%m%d`
+MY_PVR="${PVR}.${TZ}.${OS}.${ARCH}"
+
+mkdir -p "${D}/pkg/main/${PKG}.core.${MY_PVR}"
+cd "${D}/pkg/main/${PKG}.core.${MY_PVR}"
+
+mkdir gcc-config
+for pn in $(curl -s http://localhost:100/apkgdb/main?action=list | grep ^sys-devel.gcc.dev); do
+	CFG="/pkg/main/${pn}/gcc-config"
+	if [ ! -d "$CFG" ]; then
+		continue
+	fi
+	cp -vf "$CFG"/* gcc-config
+done
+
+archive
