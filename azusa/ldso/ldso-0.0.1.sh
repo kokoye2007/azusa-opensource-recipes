ac492658c46db0d120e8dadd2a483ff2be17890f
Mark Karpeles <magicaltux@gmail.com>
2021-08-09 02:03:38 +0900

    ldso: also scan 32bits libs, handle case where ldconfig is missing

--- azusa/ldso/ldso-0.0.1.sh
+++ azusa/ldso/ldso-0.0.1.sh
@@ -13,24 +13,41 @@ LIBS="lib lib32 lib64"
 mkdir etc
 ln -s /pkg/main/sys-libs.glibc.dev/etc/rpc etc/rpc
 
-for pn in $(curl -s http://localhost:100/apkgdb/main?action=list | grep -v busybox | grep libs); do
-	p=/pkg/main/${pn}
-	t=`echo "$pn" | cut -d. -f3`
-	if [ $t != "libs" ]; then
-		continue
-	fi
-	echo -ne "\rScanning: $pn\033[K"
-	for foo in $LIBS; do
-		if [ -d "${p}/$foo" -a ! -L "${p}/$foo" ]; then
-			echo "${p}/$foo" >>etc/ld.so.conf.tmp
+scanlibs() {
+	for pn in $(curl -s "http://localhost:100/apkgdb/main?action=list&sub=$1" | grep -v busybox | grep libs); do
+		p=/pkg/main/${pn}
+		t=`echo "$pn" | cut -d. -f3`
+		if [ $t != "libs" ]; then
+			continue
 		fi
+		echo -ne "\rScanning: $pn\033[K"
+		for foo in $LIBS; do
+			if [ -d "${p}/$foo" -a ! -L "${p}/$foo" ]; then
+				echo "${p}/$foo" >>etc/ld.so.conf.tmp
+			fi
+		done
 	done
-done
+}
+
+scanlibs "${OS}.${ARCH}"
+
+case $ARCH in
+	amd64)
+		# amd64 → also scan 32bits libs
+		scanlibs "${OS}.386"
+		;;
+esac
+
 # reverse order in ld.so.conf so newer versions are on top and taken in priority
 tac etc/ld.so.conf.tmp >etc/ld.so.conf
 rm etc/ld.so.conf.tmp
 
 echo "Generating ld.so.cache..."
-/pkg/main/sys-libs.glibc.core/sbin/ldconfig -X -C etc/ld.so.cache -f etc/ld.so.conf
+if [ -f /pkg/main/sys-libs.glibc.core/sbin/ldconfig ]; then
+	/pkg/main/sys-libs.glibc.core/sbin/ldconfig -X -C etc/ld.so.cache -f etc/ld.so.conf
+else
+	# can't be helped...?
+	ldconfig -X -C etc/ld.so.cache -f etc/ld.so.conf
+fi
 
 archive
