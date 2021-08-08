dd03da9b776b594cf54b819a12c116c052255074
Mark Karpeles <magicaltux@gmail.com>
2021-08-08 19:41:41 +0900

    adding patchelf

---
+++ dev-util/patchelf/patchelf-0.13.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/NixOS/${PN}/releases/download/${PV}/${P}.tar.bz2
+acheck
+
+cd "${T}"
+
+doconf
+
+make
+make install DESTDIR="${D}"
+
+finalize
