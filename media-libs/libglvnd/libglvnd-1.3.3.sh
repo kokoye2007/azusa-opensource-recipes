3e0e03f886231472a8ccdf044626d3c6565d9815
Mark Karpeles <magicaltux@gmail.com>
2021-08-10 01:13:18 +0900

    adding libglvnd-1.3.3

---
+++ media-libs/libglvnd/libglvnd-1.3.3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+# grab via git
+fetchgit https://gitlab.freedesktop.org/glvnd/libglvnd.git "v${PV}"
+acheck
+
+importpkg x11
+
+cd "${T}"
+
+domeson -Dx11=enabled -Dglx=enabled -Dtls=enabled
+
+ninja
+DESTDIR="${D}" ninja install
+
+finalize
