aa7aee36cae96dc117d8b9e3705af22642cd57a7
Mark Karpeles <magicaltux@gmail.com>
2021-08-10 00:50:11 +0900

    libesmtp compiles

--- dev-libs/libesmtp/libesmtp-1.0.6.sh
+++ dev-libs/libesmtp/libesmtp-1.0.6.sh
@@ -2,12 +2,17 @@
 source "../../common/init.sh"
 
 get http://brianstafford.info/libesmtp/${P}.tar.bz2
+acheck
 
-cd "${T}"
+importpkg openssl
 
-importpkg dev-libs/openssl
+cd "${S}"
 
-doconf --with-openssl=/pkg/main/dev-libs.openssl.dev
+# fix check for openssl
+sed 's@SSL_library_init@SSL_new@g' -i configure.ac
+aautoreconf
+
+doconf --with-openssl
 
 make
 make install DESTDIR="${D}"
