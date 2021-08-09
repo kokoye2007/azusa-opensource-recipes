e73f0dc2dccf205dc2bcd602997a4c1fa88e374d
Mark Karpeles <magicaltux@gmail.com>
2021-08-09 13:20:59 +0900

    add xxhash lib

---
+++ dev-libs/xxhash/xxhash-0.8.0.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/Cyan4973/xxHash/archive/v${PV}.tar.gz
+acheck
+
+cd "${S}"
+
+# do not define libdir as it'd prevent generation of pkg-config file, instead let finalize cleanup the thing
+MAKEOPTS=(
+	PREFIX="/pkg/main/${PKG}.core.${PVRF}"
+	#LIBDIR="/plg/main/${PKG}.libs.${PVRF}"
+	#INCLUDEDIR="/pkg/main/${PKG}.dev.${PVRF}/include"
+	datarootdir="/pkg/main/${PKG}.doc.${PVRF}"
+	DESTDIR="${D}"
+)
+
+make "${MAKEOPTS[@]}"
+make install "${MAKEOPTS[@]}"
+
+finalize
