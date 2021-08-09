7e21c1b4eb28e4ae7b5b96bfab2ba751d9e3af95
Mark Karpeles <magicaltux@gmail.com>
2021-08-09 12:40:42 +0900

    fix glib build

--- dev-libs/glib/glib-2.68.3.sh
+++ dev-libs/glib/glib-2.68.3.sh
@@ -9,8 +9,7 @@ cd "${T}"
 # TODO: eventially enable doc once we have docbook
 
 # configure & build
-meson setup --buildtype release --strip --prefix /pkg/main/${PKG}.core.${PVRF} \
-	--datadir /pkg/main/${PKG}.core.${PVRF}/share --default-library shared -Dselinux=disabled "${CHPATH}/${P}" .
+domeson --strip --default-library shared -Dselinux=disabled
 
 ninja
 DESTDIR="${D}" ninja install
