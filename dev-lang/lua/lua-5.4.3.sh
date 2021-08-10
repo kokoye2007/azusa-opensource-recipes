959c93c2b8233f3b0ea24c5dcb1ce1fd9d759667
Mark Karpeles <magicaltux@gmail.com>
2021-08-11 04:01:38 +0900

    lua: add -fPIC to cflags

--- dev-lang/lua/lua-5.4.3.sh
+++ dev-lang/lua/lua-5.4.3.sh
@@ -10,7 +10,7 @@ importpkg sys-libs/readline sys-libs/ncurses
 
 # TODO need to update luaconf.h for LUA_ROOT etc
 
-make linux PLAT=linux MYLDFLAGS="${LDFLAGS}" MYLIBS="-lncurses" INSTALL_TOP="/pkg/main/${PKG}.core.${PVRF}" INSTALL_INC="/pkg/main/${PKG}.dev.${PVRF}/include" INSTALL_LIB="/pkg/main/${PKG}.libs.${PVRF}/lib" INSTALL_MAN="/pkg/main/${PKG}.doc.${PVRF}/man/man1"
+make linux PLAT=linux MYCFLAGS="-DPIC -fPIC" MYLDFLAGS="${LDFLAGS}" MYLIBS="-lncurses" INSTALL_TOP="/pkg/main/${PKG}.core.${PVRF}" INSTALL_INC="/pkg/main/${PKG}.dev.${PVRF}/include" INSTALL_LIB="/pkg/main/${PKG}.libs.${PVRF}/lib" INSTALL_MAN="/pkg/main/${PKG}.doc.${PVRF}/man/man1"
 
 # not exactly the same values for install
 make install INSTALL_TOP="${D}/pkg/main/${PKG}.core.${PVRF}" INSTALL_INC="${D}/pkg/main/${PKG}.dev.${PVRF}/include" INSTALL_LIB="${D}/pkg/main/${PKG}.libs.${PVRF}/lib" INSTALL_MAN="${D}/pkg/main/${PKG}.doc.${PVRF}/man/man1"
