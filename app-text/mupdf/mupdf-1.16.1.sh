360e1b8af95fd0634ef959916114723b778162dd
Mark Karpeles <magicaltux@gmail.com>
2021-08-10 10:09:02 +0900

    mupdf: fixed build

--- app-text/mupdf/mupdf-1.16.1.sh
+++ app-text/mupdf/mupdf-1.16.1.sh
@@ -6,13 +6,30 @@ acheck
 
 cd "${S}"
 
-importpkg media-libs/freeglut
+PKGCONFIG="libopenjp2 glu gl glut harfbuzz"
+XCFLAGS="$(pkg-config --cflags ${PKGCONFIG}) -O2"
+XLIBS="$(pkg-config --libs ${PKGCONFIG})"
+
+echo "pkg-config: $PKGCONFIG"
+echo "XCFLAGS: $XCFLAGS"
+echo "XLIBS: $XLIBS"
 
 apatch $FILESDIR/${P}-*.patch
 
-USE_SYSTEM_LIBS=yes make
+MAKEOPTS=(
+	XCFLAGS="$XCFLAGS"
+	XLIBS="$XLIBS"
+	USE_SYSTEM_LIBS=yes
+	verbose=yes
+	DESTDIR="${D}"
+	prefix="/pkg/main/${PKG}.core.${PVRF}"
+	build=release
+	docdir="/pkg/main/${PKG}.doc.${PVRF}"
+)
 
-make DESTDIR="${D}" prefix="/pkg/main/${PKG}.core.${PVRF}" build=release docdir="/pkg/main/${PKG}.doc.${PVRF}" install
-ln -sfv mupdf-x11 "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
+make "${MAKEOPTS[@]}"
+make install "${MAKEOPTS[@]}"
+
+ln -sfv mupdf-x11 "${D}/pkg/main/${PKG}.core.${PVRF}/bin/mupdf"
 
 finalize
