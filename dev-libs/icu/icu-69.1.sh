a45369f54516ac6e2ba183bb174759579d14499c
Mark Karpeles <magicaltux@gmail.com>
2021-08-16 08:28:40 +0900

    icu: fix headers and improve compile

--- dev-libs/icu/icu-69.1.sh
+++ dev-libs/icu/icu-69.1.sh
@@ -4,16 +4,22 @@ source "../../common/init.sh"
 get https://github.com/unicode-org/icu/releases/download/release-${PV//./-}/icu4c-${PV//./_}-src.tgz
 acheck
 
-cd "icu/source"
+S="${S}/source"
 
-CC=gcc CXX=g++ doconf
+cd "${T}"
 
-make
+CC=gcc CXX=g++ doconf --disable-renaming --disable-debug --disable-samples --enable-static --disable-layoutex
+
+make -j"$NPROC" VERBOSE=1
 make install DESTDIR="${D}"
 
 # some pkgs (boost) require icu to have lib & include dirs in core, make symlinks
 ln -snfT "/pkg/main/${PKG}.libs.${PVRF}/lib" "${D}/pkg/main/${PKG}.core.${PVRF}/lib"
 ln -snfT "/pkg/main/${PKG}.libs.${PVRF}/lib$LIB_SUFFIX" "${D}/pkg/main/${PKG}.core.${PVRF}/lib$LIB_SUFFIX"
 ln -snfT "/pkg/main/${PKG}.dev.${PVRF}/include" "${D}/pkg/main/${PKG}.core.${PVRF}/include"
+
+# fix header files so that when something is included in C ansi code, it doesn't fail
+# often happens with programs linking against libxml2 which itself will include icu, comment style needs to be /* */
+sed -rie 's#// *(.*)#/* \1 */#' "${D}/pkg/main/${PKG}.dev.${PVRF}/include/unicode"/*.h
 
 finalize
