9253622163249ef4ed246c23bc0db0b5e702c908
Mark Karpeles <magicaltux@gmail.com>
2021-08-15 17:27:58 +0900

    add aria2 & gnu-efi

---
+++ net-misc/aria2/aria2-1.35.0.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/aria2/${PN}/releases/download/release-${PV}/${P}.tar.xz
+acheck
+
+cd "${T}"
+
+importpkg gmp
+
+doconf --disable-xmltest --enable-libaria2 --with-libz --with-ca-bundle="" --enable-bittorrent --enable-metalink --enable-nls --with-libcares --without-jemalloc --with-libuv --with-sqlite3 --with-libssh2 --with-tcmalloc --without-openssl --with-gnutls --with-libnettle --without-libgcrypt --with-libgmp --without-libexpat --with-libxml2
+
+make
+make install DESTDIR="${D}"
+
+finalize
---
+++ sys-boot/gnu-efi/gnu-efi-3.0.12.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://download.sourceforge.net/${PN}/${P}.tar.bz2
+acheck
+
+cd "${S}"
+
+sed -i -e "s/-Werror//" Make.defaults
+
+case "$ARCH" in
+	arm) gnuarch=arm ;;
+	arm64) gnuarch=aarch64 ;;
+	386) gnuarch=ia32 ;;
+	ia64) gnuarch=ia64 ;;
+	amd64) gnuarch=x86_64 ;;
+	*) die "Unknown arch"
+esac
+
+MAKEOPTS=(
+	ARCH="$gnuarch"
+	PREFIX="/pkg/main/${PKG}.core.${PVRF}"
+	LIBDIR="/pkg/main/${PKG}.libs.${PVRF}/lib$LIB_SUFFIX"
+	INSTALLROOT="${D}"
+)
+
+make "${MAKEOPTS[@]}"
+make install "${MAKEOPTS[@]}"
+
+finalize
