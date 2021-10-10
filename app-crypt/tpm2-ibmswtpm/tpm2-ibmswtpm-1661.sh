26e862530f1916856fafe356187bd6f5e6413a01
Mark Karpeles <magicaltux@gmail.com>
2021-10-10 22:41:09 +0900

    added tpm, bump go & openssl

---
+++ app-crypt/tpm2-ibmswtpm/tpm2-ibmswtpm-1661.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://sourceforge.net/projects/ibmswtpm2/files/ibmtpm${PV}.tar.gz
+acheck
+
+cd "${S}"
+
+# fix makefile
+sed -i 's/^CCFLAGS +=.*/CCFLAGS += $(shell pkg-config --cflags openssl)/' makefile11
+sed -i 's/LNFLAGS +=.*/LNFLAGS += $(shell pkg-config --libs openssl)/' makefile11
+
+make -f makefile11
+
+mkdir -p "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
+cp -a tpm_server "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
+cd ..
+mkdir -p "${D}/pkg/main/${PKG}.doc.${PVRF}"
+cp ibmtpm.doc LICENSE "${D}/pkg/main/${PKG}.doc.${PVRF}"
+
+finalize
