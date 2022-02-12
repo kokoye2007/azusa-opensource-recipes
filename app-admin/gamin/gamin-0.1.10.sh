ca608fbb257834a28d84fc40cfbf6f7561406b46
Mark Karpeles <magicaltux@gmail.com>
2022-02-13 07:37:23 +0900

    add gamin

---
+++ app-admin/gamin/gamin-0.1.10.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://ftp.gnome.org/pub/gnome/sources/${PN}/${PV%.*}/${P}.tar.bz2
+acheck
+
+cd "${T}"
+
+doconf --disable-debug --enable-libgamin --enable-server --enable-inotify --without-python
+
+make
+make install DESTDIR="${D}"
+
+finalize
