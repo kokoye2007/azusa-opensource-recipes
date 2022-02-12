0f86e5ffe836ab0679b72b784f176acded47ff8a
Mark Karpeles <magicaltux@gmail.com>
2022-02-13 06:38:41 +0900

    scons: bump python

--- dev-util/scons/scons-3.1.1.sh
+++ dev-util/scons/scons-3.1.1.sh
@@ -2,7 +2,7 @@
 source ../../common/init.sh
 source ${ROOTDIR}/common/python.sh
 
-PYTHON_RESTRICT="3.8"
+PYTHON_RESTRICT="3.10"
 
 get https://sourceforge.net/projects/scons/files/scons/${PV}/${P}.tar.gz
 
