0986c64e51e4ac1f4fbfeb04c8e4b9922fb768e6
Mark Karpeles <magicaltux@gmail.com>
2022-02-12 17:51:19 +0900

    fix libfmt

--- dev-libs/libfmt/libfmt-7.1.3.sh
+++ dev-libs/libfmt/libfmt-7.1.3.sh
@@ -8,7 +8,4 @@ cd "${T}"
 
 docmake -DFMT_CMAKE_DIR=/pkg/main/${PKG}.dev.${PVRF}/cmake/fmt -DFMT_LIB_DIR=/pkg/main/${PKG}.libs.${PVRF}/lib$LIB_SUFFIX -DFMT_TEST=NO
 
-make
-make install DESTDIR="${D}"
-
 finalize
