adf4460cabdcb31f7cdd876f60173267e8b48d40
Mark Karpeles <magicaltux@gmail.com>
2022-02-13 08:53:15 +0900

    fix aalib m4 warning

--- media-libs/aalib/aalib-1.4rc5.sh
+++ media-libs/aalib/aalib-1.4rc5.sh
@@ -4,6 +4,8 @@ source "../../common/init.sh"
 get https://downloads.sourceforge.net/aa-project/${P}.tar.gz
 acheck
 
+apatch "$FILESDIR/aalib-1.4_rc4-m4.patch"
+
 cd "${T}"
 
 doconflight --disable-static --with-ncurses=/pkg/main/sys-libs.ncurses.dev --with-gpm-mouse=/pkg/main/sys-libs.gpm.dev
