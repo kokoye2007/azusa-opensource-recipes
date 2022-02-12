3803c109d6ce3d8ab88bb6c108dd976569dc7983
Mark Karpeles <magicaltux@gmail.com>
2022-02-13 04:05:46 +0900

    meson python 3.10

--- dev-util/meson/meson-0.59.0.sh
+++ dev-util/meson/meson-0.59.0.sh
@@ -2,7 +2,7 @@
 source "../../common/init.sh"
 source ${ROOTDIR}/common/python.sh
 
-PYTHON_RESTRICT="3.9"
+PYTHON_RESTRICT="3.10"
 
 get https://github.com/mesonbuild/meson/releases/download/${PV}/${P}.tar.gz
 acheck
