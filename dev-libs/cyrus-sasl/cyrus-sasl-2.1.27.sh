01f4d98e695758aa22a45db341620444608f69aa
Mark Karpeles <magicaltux@gmail.com>
2022-02-13 09:01:28 +0900

    add libcrypt for cyrus-sasl

--- dev-libs/cyrus-sasl/cyrus-sasl-2.1.27.sh
+++ dev-libs/cyrus-sasl/cyrus-sasl-2.1.27.sh
@@ -6,7 +6,7 @@ acheck
 
 cd "${T}"
 
-importpkg zlib
+importpkg zlib libcrypt
 
 doconf --with-dblib=gdbm --with-gdbm=/pkg/main/sys-libs.gdbm.dev --with-openssl=/pkg/main/dev-libs.openssl.dev --with-pam=/pkg/main/sys-libs.pam.dev --with-mysql=/pkg/main/dev-db.mariadb.dev --with-pgsql=/pkg/main/dev-db.postgres.dev --with-sqlite3=/pkg/main/dev-db.sqlite.dev
 
