4def2e02404cc472ec011d6e1eff73ad1769eb51
Mark Karpeles <magicaltux@gmail.com>
2022-06-08 14:26:05 +0900

    adding build.yaml files

---
+++ sys-process/execproxy/execproxy-0.1.5.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/KarpelesLab/${PN}/archive/refs/tags/v${PV}.tar.gz
+acheck
+
+cd "${S}"
+
+make CC=musl-gcc
+make install PREFIX="/pkg/main/${PKG}.core.${PVRF}" DESTDIR="${D}"
+
+finalize
