edbb2cc7e2b0d6f3bbb37e60c9423c73ad13250b
Mark Karpeles <magicaltux@gmail.com>
2022-08-11 23:50:44 +0900

    various updates

---
+++ app-shells/zsh/zsh-5.9.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://www.zsh.org/pub/${P}.tar.xz
+acheck
+
+cd "${T}"
+
+importpkg sys-libs/ncurses
+
+doconf --enable-function-subdirs --with-tcsetpgrp --with-term-lib='tinfow ncursesw' --enable-maildir-support --enable-pcre --enable-cap --enable-multibyte --enable-gdbm
+
+# --enable-runhelpdir="${EPREFIX}"/usr/share/zsh/${PV%_*}/help
+# --enable-fndir="${EPREFIX}"/usr/share/zsh/${PV%_*}/functions
+# --enable-site-fndir="${EPREFIX}"/usr/share/zsh/site-functions
+
+make
+make install DESTDIR="${D}"
+
+finalize
---
+++ dev-util/bazel/bazel-5.0.0.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/bazelbuild/bazel/releases/download/${PV}/${P}-dist.zip
+acheck
+
+export JAVA_HOME="/pkg/main/dev-java.openjdk.core.linux.${ARCH}" # so keepwork works
+export EXTRA_BAZEL_ARGS="--jobs=$(nproc) --java_runtime_version=local_jdk --tool_java_runtime_version=local_jdk"
+
+VERBOSE=yes ./compile.sh
+
+#./scripts/generate_bash_completion.sh --bazel=output/bazel --prepend=scripts/bazel-complete-header.bash --prepend=scripts/bazel-complete-template.bash
+
+mkdir -p "${S}/pkg/main/${PKG}.core.${PVRF}/bin"
+install -v -m0755 -D -T output/bazel "${D}/pkg/main/${PKG}.core.${PVRF}/bin/bazel"
+# newbashcomp bazel-complete.bash
+# zsh?
+
+# tools?
+
+# we do not use finalize because fixelf corrupts bazel exe (which is actually a zip file concatenated with the exe)
+archive
