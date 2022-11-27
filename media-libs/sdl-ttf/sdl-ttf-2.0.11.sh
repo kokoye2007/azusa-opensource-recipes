ad73435d93f8d4de944860b01ce1e435c869c2d1
Mark Karpeles <magicaltux@gmail.com>
2022-11-28 00:49:19 +0900

    add sdl-ttf-2.0.11

---
+++ media-libs/sdl-ttf/sdl-ttf-2.0.11.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+MY_P="${P/sdl-/SDL_}"
+get http://www.libsdl.org/projects/SDL_ttf/release/${MY_P}.tar.gz
+acheck
+
+importpkg X media-libs/libjpeg-turbo media-libs/tiff
+
+cd "${T}"
+
+doconf --disable-static --with-X
+
+make -j"$NPROC"
+make install DESTDIR="${D}"
+
+finalize
