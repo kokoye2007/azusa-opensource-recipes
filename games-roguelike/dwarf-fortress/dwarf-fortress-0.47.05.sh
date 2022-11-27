26e0e3fefe619e4b7f5b07ebf06ee1ae9b60877e
Mark Karpeles <magicaltux@gmail.com>
2022-11-28 01:10:09 +0900

    fix df

--- games-roguelike/dwarf-fortress/dwarf-fortress-0.47.05.sh
+++ games-roguelike/dwarf-fortress/dwarf-fortress-0.47.05.sh
@@ -7,6 +7,9 @@ MY_PV="${MY_PV/./_}"
 get http://www.bay12games.com/dwarves/df_${MY_PV}_linux.tar.bz2
 acheck
 
+# TODO we should be able to re-build libgraphics.so
+
+rm df_linux/libs/{libgcc_s.so.1,libstdc++.so.6}
 mkdir -p "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
 mv df_linux "${D}/pkg/main/${PKG}.data.${PVRF}"
 cat >"${D}/pkg/main/${PKG}.core.${PVRF}/bin/dwarf-fortress" <<EOF
@@ -14,5 +17,6 @@ cat >"${D}/pkg/main/${PKG}.core.${PVRF}/bin/dwarf-fortress" <<EOF
 cd "/pkg/main/${PKG}.data.${PVRF}"
 exec ./df "$@"
 EOF
+chmod +x "${D}/pkg/main/${PKG}.core.${PVRF}/bin/dwarf-fortress"
 
 finalize
