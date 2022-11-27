7a2e1dde385eb925e0b11487584c83ccef2d7d48
Mark Karpeles <magicaltux@gmail.com>
2022-11-28 01:29:40 +0900

    try to make it work

--- games-roguelike/dwarf-fortress/dwarf-fortress-0.47.05.sh
+++ games-roguelike/dwarf-fortress/dwarf-fortress-0.47.05.sh
@@ -12,10 +12,27 @@ acheck
 rm df_linux/libs/{libgcc_s.so.1,libstdc++.so.6}
 mkdir -p "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
 mv df_linux "${D}/pkg/main/${PKG}.data.${PVRF}"
+
+# script inspired from the one used by archlinux
 cat >"${D}/pkg/main/${PKG}.core.${PVRF}/bin/dwarf-fortress" <<EOF
 #!/bin/bash
-cd "/pkg/main/${PKG}.data.${PVRF}"
-exec ./df "$@"
+export SDL_DISABLE_LOCK_KEYS=1 # Work around for bug in Debian/Ubuntu SDL patch.
+#export SDL_VIDEO_CENTERED=1    # Centre the screen.  Messes up resizing.
+
+if [[ ! -d "\$HOME/.cache/dwarf-fortress" ]; then
+	mkdir -p "\$HOME/.cache/dwarf-fortress/data"
+fi
+
+# ln -snf overwrites symlinks
+ln -snf "/pkg/main/${PKG}.data.${PVRF}/raw" "\$HOME/.cache/dwarf-fortress/raw"
+ln -snf "/pkg/main/${PKG}.data.${PVRF}/libs" "\$HOME/.cache/dwarf-fortress/libs"
+ln -snf "/pkg/main/${PKG}.data.${PVRF}/data/init" "\$HOME/.cache/dwarf-fortress/data/init"
+
+for link in announcement art dipscript help index initial_movies movies shader.fs shader.vs sound speech; do
+	cp -ru "/pkg/main/${PKG}.data.${PVRF}/data/\$link" -t "\$HOME/.cache/dwarf-fortress/data/"
+done
+cd "\$HOME/.cache/dwarf-fortress"
+exec ./libs/Dwarf_Fortress "$@"
 EOF
 chmod +x "${D}/pkg/main/${PKG}.core.${PVRF}/bin/dwarf-fortress"
 
