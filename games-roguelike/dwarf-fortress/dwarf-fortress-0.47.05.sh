a4237edffb37e41a19da8b5eca818a86c4081d71
Mark Karpeles <magicaltux@gmail.com>
2022-11-27 18:49:03 +0900

    fix package

--- games-roguelike/dwarf-fortress/dwarf-fortress-0.47.05.sh
+++ games-roguelike/dwarf-fortress/dwarf-fortress-0.47.05.sh
@@ -7,11 +7,11 @@ MY_PV="${MY_PV/./_}"
 get http://www.bay12games.com/dwarves/df_${MY_PV}_linux.tar.bz2
 acheck
 
-mkdir -p "${D}/pkg/${PKG}.core.${PVRF}/bin"
-mv df_linux "${D}/pkg/${PKG}.data.${PVRF}"
-cat >"${D}/pkg/${PKG}.core.${PVRF}/bin/dwarf-fortress" <<<EOF
+mkdir -p "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
+mv df_linux "${D}/pkg/main/${PKG}.data.${PVRF}"
+cat >"${D}/pkg/main/${PKG}.core.${PVRF}/bin/dwarf-fortress" <<EOF
 #!/bin/bash
-cd "/pkg/${PKG}.data.${PVRF}"
+cd "/pkg/main/${PKG}.data.${PVRF}"
 exec ./df "$@"
 EOF
 
