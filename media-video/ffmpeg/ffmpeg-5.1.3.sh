bc591ef1c91102d71f2585916a078ddffb461ce8
Mark Karpeles <magicaltux@gmail.com>
2023-04-07 00:23:45 +0900

    make nvenc work?

--- media-video/ffmpeg/ffmpeg-5.1.3.sh
+++ media-video/ffmpeg/ffmpeg-5.1.3.sh
@@ -6,7 +6,7 @@ acheck
 
 cd "${T}"
 
-importpkg dev-libs/gmp media-sound/gsm media-sound/lame theora media-libs/xvid libgcrypt libmodplug icu-uc media-video/avisynth dev-util/nvidia-cuda-toolkit
+importpkg dev-libs/gmp media-sound/gsm media-sound/lame theora media-libs/xvid libgcrypt libmodplug icu-uc media-video/avisynth dev-util/nvidia-cuda-toolkit media-libs/nv-codec-headers
 
 callconf --prefix=/pkg/main/${PKG}.core.${PVRF} \
 	--libdir=/pkg/main/${PKG}.libs.${PVRF}/lib$LIB_SUFFIX \
@@ -16,7 +16,7 @@ callconf --prefix=/pkg/main/${PKG}.core.${PVRF} \
 	--enable-avisynth --enable-gmp --enable-version3 --enable-gcrypt --enable-openssl --enable-libgsm --enable-libmp3lame --enable-libmodplug \
 	--enable-libopencv --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx264 --enable-libxvid \
 	--enable-libxml2 --enable-libx265 --enable-libfreetype \
-	--enable-cuvid --enable-libnpp --enable-vaapi --enable-cuda-nvcc --nvccflags='-gencode arch=compute_80,code=sm_80 -O2 -I /pkg/main/dev-util.nvidia-cuda-toolkit.dev/include' --enable-libnpp --enable-nvenc || /bin/bash -i
+	--enable-cuvid --enable-libnpp --enable-ffnvcodec --enable-vaapi --enable-cuda-nvcc --nvcc=/pkg/main/dev-util.nvidia-cuda-toolkit.core/bin/nvcc --nvccflags='-gencode arch=compute_80,code=sm_80 -O2 -I /pkg/main/dev-util.nvidia-cuda-toolkit.dev/include' --enable-libnpp --enable-nvenc
 #--enable-libass --enable-libbluray --enable-libdrm --enable-libkvazaar --enable-libxavs --enable-libxavs2
 
 make
