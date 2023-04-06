1a4565887f302f25813d9edda45b04ffc132d6d4
Mark Karpeles <magicaltux@gmail.com>
2023-04-07 01:14:32 +0900

    add cudnn, libassuan azusa.yaml and bortli metadata

---
+++ dev-libs/cudnn/cudnn-8.6.0.163.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+# supports 11.x but URL has a specific version number
+CUDA_MA="11"
+CUDA_MI="8"
+CUDA_V="${CUDA_MA}.${CUDA_MI}"
+
+get https://developer.download.nvidia.com/compute/redist/cudnn/v${PV%.*}/local_installers/${CUDA_V}/cudnn-linux-x86_64-${PV}_cuda${CUDA_MA}-archive.tar.xz
+acheck
+
+cd "${S}"
+
+mkdir -pv "${D}/pkg/main/${PKG}.core.${PVRF}"
+
+mv -v * "${D}/pkg/main/${PKG}.core.${PVRF}"
+
+finalize
