8f2a622dda9ee4b85439a528d3f5e03198e61e71
Mark Karpeles <magicaltux@gmail.com>
2023-04-07 02:31:39 +0900

    allow overriding of cuda lib path

--- media-libs/nv-codec-headers/nv-codec-headers-11.1.5.2.sh
+++ media-libs/nv-codec-headers/nv-codec-headers-11.1.5.2.sh
@@ -4,6 +4,8 @@ source "../../common/init.sh"
 get https://github.com/FFmpeg/nv-codec-headers/releases/download/n${PV}/${P}.tar.gz
 acheck
 
+apatch "$FILESDIR/nv-codec-headers-11.1.5.2-cuda_lib_prefix.patch"
+
 cd "${S}"
 
 make PREFIX="/pkg/main/${PKG}.dev.${PVRF}" LIBDIR=
