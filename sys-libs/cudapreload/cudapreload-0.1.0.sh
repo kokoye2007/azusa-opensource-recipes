939f0341109c029ae88821a03f047281b873a74b
Mark Karpeles <magicaltux@gmail.com>
2023-04-15 00:25:24 +0900

    add cudapreload

---
+++ sys-libs/cudapreload/cudapreload-0.1.0.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/AzusaOS/${PN}/archive/refs/tags/v${PV}.tar.gz ${P}.tar.gz
+acheck
+
+cd "${S}"
+
+mkdir -p "${D}/pkg/main/${PKG}.dev.${PVRF}/lib$LIB_SUFFIX"
+if [ x"$LIB_SUFFIX" != x ]; then
+	ln -snf "lib$LIB_SUFFIX" "${D}/pkg/main/${PKG}.dev.${PVRF}/lib"
+fi
+
+make
+make install PREFIX="/pkg/main/${PKG}.dev.${PVRF}" LIBDIR="lib$LIB_SUFFIX" DESTDIR="${D}"
+
+archive
