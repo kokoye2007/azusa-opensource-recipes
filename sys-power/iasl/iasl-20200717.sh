12f5797ec5e93e5baefe96fbfaf319353006037c
Mark Karpeles <magicaltux@gmail.com>
2023-04-15 14:53:48 +0900

    add sys-power/iasl

---
+++ sys-power/iasl/iasl-20200717.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+MY_PN=acpica-unix
+MY_P="${MY_PN}-${PV}"
+get http://www.acpica.org/sites/acpica/files/${MY_P}.tar.gz
+cd "${S}"
+apatch "$FILESDIR/iasl-20200326-Makefile.patch"
+find "${S}" -type f -name 'Makefile*' -print0 | \
+	xargs -0 -I '{}' \
+	sed -r -e 's:-\<Werror\>::g' -e "s:/usr:${EPREFIX}/usr:g" \
+	-i '{}'
+acheck
+
+cd "${S}"
+
+make -C generate/unix PREFIX="/pkg/main/${PKG}.core.${PVRF}" BITS="${BITS}"
+
+cd generate/unix
+
+make install PREFIX="/pkg/main/${PKG}.core.${PVRF}" DESTDIR="${D}" BITS=${BITS}
+
+finalize
