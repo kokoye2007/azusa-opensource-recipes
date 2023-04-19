49f4c12c2d14ba13139299d4e8398e11bd1a9a63
Mark Karpeles <magicaltux@gmail.com>
2023-04-20 00:59:10 +0900

    add nikto 202209

---
+++ net-analyzer/nikto/nikto-2.1.6.20220919.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+fetchgit https://github.com/sullo/nikto.git fed0c5cab5ed00c4b8f07a781a8fcde25fe3bce2
+acheck
+
+cd "${S}"
+
+mkdir -pv "${D}/pkg/main/${PKG}.core.${PVRF}"
+mv -v program "${D}/pkg/main/${PKG}.core.${PVRF}/nikto"
+
+chmod +x "${D}/pkg/main/${PKG}.core.${PVRF}/nikto/nikto.pl"
+mkdir -pv "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
+ln -snf ../nikto/nikto.pl "${D}/pkg/main/${PKG}.core.${PVRF}/bin/nikto"
+
+mkdir -pv "${D}/pkg/main/${PKG}.doc.${PVRF}"
+mv -v devdocs documentation README.md "${D}/pkg/main/${PKG}.doc.${PVRF}"
+
+archive
