26f96ff1162752a2b395af2cf35af0544311498e
Mark Karpeles <magicaltux@gmail.com>
2023-08-23 00:42:10 +0900

    solidity fixes

--- dev-lang/solidity/solidity-0.8.21.sh
+++ dev-lang/solidity/solidity-0.8.21.sh
@@ -1,45 +1,57 @@
 #!/bin/sh
 source "../../common/init.sh"
 
+COMMIT_HASH="d9974bed7134e043f7ccc593c0c19c67d2d45dc4"
+JSONCPP_VERSION="1.9.3" # solidity depends on an exact version of jsoncpp for some reason
 get https://github.com/ethereum/solidity/archive/refs/tags/v${PV}.tar.gz "${P}.tar.gz"
 acheck
 
 cd "${S}"
 
+# configure for release version
+echo "$COMMIT_HASH" >commit_hash.txt
+echo -n >prerelease.txt
+
 # prevent solidity from trying to download third party software
 echo 'find_package(fmt REQUIRED)' >cmake/fmtlib.cmake
 echo 'find_package(jsoncpp REQUIRED)' >cmake/jsoncpp.cmake
 echo 'find_package(range-v3 REQUIRED)' >cmake/range-v3.cmake
-echo 'd9974bed7134e043f7ccc593c0c19c67d2d45dc4' >commit_hash.txt
-echo -n >prerelease.txt
+
+# force SOVERSION on libs to ensure a given solc uses the right libs
+for foo in evmasm langutil smtutil libsolc solidity solutil yul; do
+	if [ -d lib$foo ]; then
+		echo "set_target_properties($foo PROPERTIES VERSION $PV SOVERSION $PV)" >>lib$foo/CMakeLists.txt
+	else
+		echo "set_target_properties($foo PROPERTIES VERSION $PV SOVERSION $PV)" >>$foo/CMakeLists.txt
+	fi
+done
+echo "set_target_properties(phaser PROPERTIES VERSION $PV SOVERSION $PV)" >>tools/CMakeLists.txt
+echo "set_target_properties(solcli PROPERTIES VERSION $PV SOVERSION $PV)" >>solc/CMakeLists.txt
 
 cd "${T}"
 
+# ensure solidity can find z3
 importpkg sci-mathematics/z3
 
-export CPPFLAGS="$CPPFLAGS -I/pkg/main/dev-libs.jsoncpp.dev.1.9.3/include/jsoncpp"
-export LDFLAGS="$LDFLAGS -L/pkg/main/dev-libs.jsoncpp.libs.1.9.3/lib$LIB_SUFFIX"
+export CPPFLAGS="$CPPFLAGS -I/pkg/main/dev-libs.jsoncpp.dev.$JSONCPP_VERSION/include/jsoncpp"
+export LDFLAGS="$LDFLAGS -L/pkg/main/dev-libs.jsoncpp.libs.$JSONCPP_VERSION/lib$LIB_SUFFIX"
 
 CMAKEOPTS=(
 	-DUSE_CVC4=OFF
 	-DTESTS=OFF
 
-	-Djsoncpp_ROOT=/pkg/main/dev-libs.jsoncpp.dev.1.9.3
+	-Djsoncpp_ROOT=/pkg/main/dev-libs.jsoncpp.dev.$JSONCPP_VERSION
 
+	-DBoost_ROOT=/pkg/main/dev-libs.boost.dev
 	-DBoost_NO_WARN_NEW_VERSIONS=1
 	-DBoost_USE_STATIC_LIBS=OFF
-	-DBOOST_ROOT=/pkg/main/dev-libs.boost.dev
-	-DBOOST_INCLUDEDIR=/pkg/main/dev-libs.boost.dev/include/boost
-	-DBOOST_LIBRARYDIR=/pkg/main/dev-libs.boost.libs/lib$LIB_SUFFIX
-	-DBoost_NO_SYSTEM_PATHS=ON
 )
 
 docmake "${CMAKEOPTS[@]}"
 
 mkdir -p "${D}/pkg/main/${PKG}.libs.${PVRF}/lib$LIB_SUFFIX"
-for foo in */*.so; do
-	tgt="$(basename "$foo")"
-	cp -v "$foo" "${D}/pkg/main/${PKG}.libs.${PVRF}/lib$LIB_SUFFIX/$tgt.${PV}"
+for foo in */*.so.${PV}; do
+	cp -v "$foo" "${D}/pkg/main/${PKG}.libs.${PVRF}/lib$LIB_SUFFIX/"
 done
 
 finalize
