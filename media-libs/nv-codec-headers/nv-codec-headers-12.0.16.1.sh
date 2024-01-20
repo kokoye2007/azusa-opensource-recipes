9f7eead1d7762778790f1b1d98faca7c191be433
Mark Karpeles <magicaltux@gmail.com>
2024-01-20 17:06:20 +0900

    add nv-codec-headers-12.0.16.1

---
+++ media-libs/nv-codec-headers/nv-codec-headers-12.0.16.1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/FFmpeg/nv-codec-headers/releases/download/n${PV}/${P}.tar.gz
+acheck
+
+cd "${S}"
+
+apatch "$FILESDIR/nv-codec-headers-11.1.5.2-cuda_lib_prefix.patch"
+
+make PREFIX="/pkg/main/${PKG}.dev.${PVRF}" LIBDIR=
+make DESTDIR="${D}" PREFIX="/pkg/main/${PKG}.dev.${PVRF}" LIBDIR= install
+
+finalize
