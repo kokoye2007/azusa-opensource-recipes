0f384a0ef08b18cf4f75d775628fcc2780c7b046
Mark Karpeles <magicaltux@gmail.com>
2024-02-17 16:17:05 +0900

    bump glibc

--- media-video/ffmpeg/ffmpeg-6.1.1.sh
+++ media-video/ffmpeg/ffmpeg-6.1.1.sh
@@ -20,9 +20,9 @@ callconf --prefix=/pkg/main/${PKG}.core.${PVRF} \
 	--enable-libopencv --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx264 --enable-libxvid \
 	--enable-libxml2 --enable-libx265 --enable-libfreetype \
 	--enable-cuvid --enable-libnpp --enable-ffnvcodec --enable-vaapi --enable-cuda-nvcc \
-	--enable-libdav1d \
+	--enable-libdav1d --enable-libass \
 	--nvcc=/pkg/main/dev-util.nvidia-cuda-toolkit.core/bin/nvcc --nvccflags='-gencode arch=compute_80,code=sm_80 -O2 --compiler-bindir /pkg/main/sys-devel.gcc.core.12/bin/ -I /pkg/main/dev-util.nvidia-cuda-toolkit.dev/include' --enable-libnpp --enable-nvenc
-#--enable-libass --enable-libbluray --enable-libdrm --enable-libkvazaar --enable-libxavs --enable-libxavs2
+# --enable-libbluray --enable-libdrm --enable-libkvazaar --enable-libxavs --enable-libxavs2
 ## --enable-librav1e fails because rust
 
 make
