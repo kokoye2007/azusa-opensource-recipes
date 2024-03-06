65fc77dc41f6cdd06aa1e2a1baa980dc2e542343
Mark Karpeles <magicaltux@gmail.com>
2024-03-07 00:46:40 +0900

    bump go

---
+++ dev-lang/go/go-1.22.1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://dl.google.com/${PN}/go${PV}.src.tar.gz
+
+cd "go/src"
+
+export GOOS="${OS}"
+export GOARCH="${ARCH}"
+export GOROOT_FINAL="/pkg/main/${PKG}.dev.${PVRF}"
+export GOROOT="${D}/pkg/main/${PKG}.dev.${PVRF}"
+export PATH="/pkg/main/dev-lang.go.dev/bin/:$PATH"
+if [ x"$ARCH" = x"386" ]; then
+	export CGO_CFLAGS=-fno-stack-protector # https://github.com/golang/go/issues/54313
+fi
+
+echo "Using: GOOS=$GOOS GOARCH=$GOARCH GOROOT_FINAL=$GOROOT_FINAL"
+
+./all.bash
+
+# install
+cd ../..
+mkdir -p "${D}/pkg/main/"
+mv go "${D}/pkg/main/${PKG}.dev.${PVRF}"
+
+finalize
