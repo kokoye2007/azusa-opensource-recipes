53c68ce002a94a0e657cc68be1efbc6ebff24255
Mark Karpeles <magicaltux@gmail.com>
2024-03-10 15:23:49 +0900

    add dlib

---
+++ sci-libs/dlib/dlib-19.24.2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/davisking/dlib/archive/refs/tags/v${PV}.tar.gz $P.tar.gz
+acheck
+
+cd "${T}"
+
+importpkg X
+#importpkg dev-util/nvidia-cuda-toolkit dev-libs/cudnn X
+
+# somehow fails with cuda
+docmake -DDLIB_LINK_WITH_SQLITE3=ON -DDLIB_USE_CUDA=OFF
+
+finalize
