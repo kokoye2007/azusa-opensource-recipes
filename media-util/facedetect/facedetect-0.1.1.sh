24960c252563f47d1fe1456275afe2e650348640
Mark Karpeles <magicaltux@gmail.com>
2024-03-13 21:19:07 +0900

    add facedetect

---
+++ media-util/facedetect/facedetect-0.1.1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/AzusaOS/facedetect/archive/refs/tags/v0.1.1.tar.gz $P.tar.gz
+acheck
+
+cd "${S}"
+
+mkdir -p "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
+cp -a facedetect "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
+
+finalize
