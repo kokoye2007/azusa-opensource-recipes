a518c08b06f150cb3a9f3caf4cfe985dd93c04d9
Mark Karpeles <magicaltux@gmail.com>
2024-03-13 21:19:22 +0900

    add directx-headers

---
+++ dev-util/directx-headers/directx-headers-1.610.2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+MY_PN=DirectX-Headers
+get https://github.com/microsoft/${MY_PN}/archive/refs/tags/v${PV}.tar.gz ${P}.tar.gz
+acheck
+
+cd "${T}"
+
+domeson -Dbuild-test=false
+
+finalize
