0dcd7adbe7b57a1be6a372a1823c5212d534ea8e
Mark Karpeles <magicaltux@gmail.com>
2024-03-14 21:36:50 +0900

    fix xformers log as it requires cuda

--- dev-python/xformers/xformers-0.0.24.sh
+++ dev-python/xformers/xformers-0.0.24.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 source ../../common/init.sh
 inherit python
+inherit cuda
+
+# empty CPPFLAGS since nvcc won't support -pipe etc
+CPPFLAGS=""
+
+initcuda 11.8
+
+importpkg sci-libs/caffe2 dev-cpp/gflags
+
+# https://github.com/pytorch/pytorch/blob/main/torch/utils/cpp_extension.py#L1959
+export TORCH_CUDA_ARCH_LIST="7.5;8.0;9.0+PTX"
+
+# pytorch will not honor CPPFLAGS, so pass that directly to nvcc instead
+export PYTORCH_NVCC="${CUDA_HOME}/bin/nvcc ${CPPFLAGS}"
 
 python_do_standard_package
