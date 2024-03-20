5ba862b7e9ba0af02b52e23da31552611c54d465
Mark Karpeles <magicaltux@gmail.com>
2024-03-20 09:33:44 +0900

    xformers cuda set to 12.1

--- dev-python/xformers/xformers-0.0.24.sh
+++ dev-python/xformers/xformers-0.0.24.sh
@@ -6,17 +6,17 @@ inherit cuda
 # empty CPPFLAGS since nvcc won't support -pipe etc
 CPPFLAGS=""
 
-initcuda 11.8
+initcuda 12.1
 
 importpkg sci-libs/caffe2 dev-cpp/gflags
 
 # https://github.com/pytorch/pytorch/blob/main/torch/utils/cpp_extension.py#L1959
-export TORCH_CUDA_ARCH_LIST="7.5;8.0;9.0+PTX"
+export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;9.0+PTX"
 
 # pytorch will not honor CPPFLAGS, so pass that directly to nvcc instead
 export PYTORCH_NVCC="${CUDA_HOME}/bin/nvcc ${CPPFLAGS}"
 
 # avoid computer to die
-export MAX_JOBS=1
+export MAX_JOBS=4
 
 python_do_standard_package
