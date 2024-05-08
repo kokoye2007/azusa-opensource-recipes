b540a4ddbd10cc044f0a57f8f3c478ff1b0b8608
Mark Karpeles <magicaltux@gmail.com>
2024-05-08 14:42:54 +0900

    add script to switch gcc version

---
+++ common/gcc.sh
@@ -0,0 +1,9 @@
+# gcc
+
+switchgcc() {
+	local GCC_VERSION="$1"
+	local GCC_PATH="$(realpath /pkg/main/sys-devel.gcc.core.$GCC_VERSION/bin)"
+	echo " * Switching to gcc-$GCC_VERSION at $GCC_PATH"
+	export PATH="$GCC_PATH:$PATH"
+	rm -f /usr/bin/gcc /usr/bin/g++
+}
