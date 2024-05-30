081c604b40fa427265d660342105bfa8af720b3f
Mark Karpeles <magicaltux@gmail.com>
2024-05-30 12:42:10 +0900

    add geth

---
+++ net-p2p/go-ethereum/go-ethereum-1.14.3.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+source "../../common/init.sh"
+get https://github.com/ethereum/go-ethereum/archive/refs/tags/v${PV}.tar.gz ${P}.tar.gz
+envcheck
+# do not use acheck so we keep networking
+
+export PATH="/pkg/main/dev-lang.go.dev.1.21/bin:$PATH"
+
+cd "${S}"
+
+echo -n "Using: "
+go version
+
+make all
+
+mkdir -pv "${D}/pkg/main/${PKG}.core.${PVRF}"
+mv -v build/bin "${D}/pkg/main/${PKG}.core.${PVRF}"
+
+finalize
