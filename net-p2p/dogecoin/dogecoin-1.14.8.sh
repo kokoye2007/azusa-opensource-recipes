32f12b0b20928fc4d7c680ec66c0f16dba38dcc2
Mark Karpeles <magicaltux@gmail.com>
2024-09-26 19:37:18 +0900

    add dogecoin

---
+++ net-p2p/dogecoin/dogecoin-1.14.8.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/dogecoin/dogecoin/releases/download/v${PV}/dogecoin-${PV}.tar.gz
+acheck
+
+cd "${S}"
+
+aautoreconf
+
+cd "${T}"
+
+importpkg dev-libs/boost dev-libs/libevent sys-libs/db:5.3 net-libs/miniupnpc
+
+doconf --with-boost-libdir="/pkg/main/dev-libs.boost.libs/lib$LIB_SUFFIX" --enable-asm --without-qtdbus --without-qrencode --enable-wallet --with-daemon --disable-bench --without-libs --without-gui --without-rapidcheck --disable-fuzz --disable-ccache --disable-static --with-system-libsecp256k1 --with-system-univalue
+#--with-miniupnpc --enable-upnp-default --enable-zmq --disable-util-cli --disable-util-tx --disable-util-wallet
+
+make -j"$NPROC"
+make install DESTDIR="${D}"
+
+finalize
