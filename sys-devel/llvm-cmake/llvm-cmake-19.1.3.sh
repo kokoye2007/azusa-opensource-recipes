e207c4db4066fb7885d5e0b06405c5f7fa1e8b4c
Mark Karpeles <magicaltux@gmail.com>
2024-11-19 10:57:42 +0900

    bump cockroach & llvm-cmakle

---
+++ sys-devel/llvm-cmake/llvm-cmake-19.1.3.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/llvm/llvm-project/releases/download/llvmorg-${PV}/cmake-${PV}.src.tar.xz
+acheck
+
+mkdir -pv "${D}/pkg/main/${PKG}.src.${PVR}.any.any"
+mv -v "$S" "${D}/pkg/main/${PKG}.src.${PVR}.any.any/cmake"
+
+archive
