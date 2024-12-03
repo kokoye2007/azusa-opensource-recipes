4ad8e5268b4dbd806ac91e4ecdad0010a4c6497e
kokoye2007 <kokoye2007@gmail.com>
2024-12-03 16:47:36 +1100

    chrome 131 and change url
    Signed-off-by: kokoye2007 <kokoye2007@gmail.com>

---
+++ www-client/google-chrome/google-chrome-131.0.6778.69.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+# in order to know latest version:
+# https://versionhistory.googleapis.com/v1/chrome/platforms/linux/channels/stable/versions
+# get_version.sh
+
+get https://dl.google.com/linux/chrome/deb/pool/main/g/"${PN}"-stable/"${PN}"-stable_"${PV}"-1_amd64.deb
+
+acheck
+
+ar x "${PN}-stable_${PV}-1_amd64.deb"
+
+tar xvf data.tar.xz
+
+rm -r etc
+
+# todo ... a lot of stuff...
+
+# move language packs to separate packages (+symlinks)
+
+mkdir -p "${D}/pkg/main/${PKG}.core.${PVRF}"
+mv usr/* "${D}/pkg/main/${PKG}.core.${PVRF}"
+mv opt/google/chrome "${D}/pkg/main/${PKG}.core.${PVRF}"
+
+# set suid bit on chrome-sandbox, and limit to +x
+chmod -v 04711 "${D}/pkg/main/${PKG}.core.${PVRF}/chrome/chrome-sandbox"
+
+cd "${D}/pkg/main/${PKG}.core.${PVRF}" || exit
+rm bin/google-chrome-stable
+
+ln -s /pkg/main/"${PKG}".core."${PVRF}"/chrome/google-chrome bin/google-chrome-stable
+
+# ...
+
+finalize
