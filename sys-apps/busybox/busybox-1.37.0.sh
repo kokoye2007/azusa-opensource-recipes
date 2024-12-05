b34eb0cf4f475a18f057d024ca327c4ef768de9e
kokoye2007 <kokoye2007@gmail.com>
2024-12-05 15:17:32 +1100

    sys-app/busybox 1.37.0
    Signed-off-by: kokoye2007 <kokoye2007@gmail.com>

---
+++ sys-apps/busybox/busybox-1.37.0.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://busybox.net/downloads/${P}.tar.bz2
+acheck
+
+echo "Compiling ${P} ..."
+cd "${P}"
+cp $FILESDIR/config-${PV} .config
+
+make CC="musl-gcc -I /pkg/main/sys-kernel.linux.dev/include"
+make install CC="musl-gcc -I /pkg/main/sys-kernel.linux.dev/include"
+
+mkdir -p "${D}/pkg/main/${PKG}.core.${PVRF}/"
+rsync -a ./_install/ "${D}/pkg/main/${PKG}.core.${PVRF}/"
+mkdir -p "${D}/pkg/main/${PKG}.doc.${PVRF}/"
+rsync -av --progress ./examples "${D}/pkg/main/${PKG}.doc.${PVRF}/"
+
+finalize
