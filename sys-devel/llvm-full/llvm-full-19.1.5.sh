ed6f19255d3f1ef54bd37291c9d58f24b5e9a43b
Mark Karpeles <magicaltux@gmail.com>
2024-12-15 13:32:46 +0900

    fix llvm bootstrap & add llvm-full

--- sys-devel/llvm-full/llvm-full-19.1.5.sh
+++ sys-devel/llvm-full/llvm-full-19.1.5.sh
@@ -1,32 +1,19 @@
 #!/bin/sh
 source "../../common/init.sh"
-# this is only the base llvm
+inherit llvm
 
-CATEGORY=sys-devel PN=llvm get https://github.com/llvm/llvm-project/releases/download/llvmorg-${PV}/llvm-project-${PV}.src.tar.xz
+llvmget _llvm
 acheck
-
-S="$S/llvm"
 
 cd "${T}"
 
-importpkg libxml-2.0 icu-uc sci-mathematics/z3 zlib dev-libs/libedit
+importpkg libxml-2.0 icu-uc sci-mathematics/z3 zlib sys-libs/llvm-libunwind sys-libs/libcxx sys-libs/libcxxabi app-arch/xz sys-libs/ncurses dev-lang/lua
+# -- Could NOT find CursesAndPanel (missing: CURSES_INCLUDE_DIRS CURSES_LIBRARIES PANEL_LIBRARIES) 
+# -- Could NOT find Lua (missing: LUA_LIBRARIES LUA_INCLUDE_DIR) (Required is exact version "5.3")
+
 
 # Testing the c++ compiler:
-# echo -e '#include <iostream>\nint main() { std::cout << "hello world" << std::endl; return 0; }' | /pkg/main/sys-devel.llvm-bootstrap.data/bin/clang++ -x c++ -o test -
-
-# somehow, clang fails to find the system includes at some point
-#export CPPFLAGS="${CPPFLAGS} -isystem /pkg/main/sys-libs.glibc.dev.${OS}.${ARCH}/include"
-export C_INCLUDE_PATH="$C_INCLUDE_PATH:/pkg/main/sys-libs.glibc.dev.${OS}.${ARCH}/include"
-
-# do we already have a boostrapped llvm?
-if [ -d /pkg/main/sys-devel.llvm-bootstrap.data ]; then
-	#export CC=/pkg/main/sys-devel.llvm-bootstrap.data/bin/clang
-	# ensure -lc++ can be found
-	export LIBRARY_PATH="${LIBRARY_PATH}:/pkg/main/sys-devel.llvm-bootstrap.data/lib$LIB_SUFFIX/$CHOST"
-else
-	# fallback on hopefully installed libcxx
-	export LIBRARY_PATH="${LIBRARY_PATH}:/pkg/main/sys-libs.libcxx.libs/lib$LIB_SUFFIX:/pkg/main/sys-libs.libcxxabi.libs/lib$LIB_SUFFIX"
-fi
+# echo -e '#include <iostream>\nint main() { std::cout << "hello world" << std::endl; return 0; }' | /pkg/main/sys-devel.llvm-full.data/bin/clang++ -x c++ -o test -
 
 # importpkg will set CPPFLAGS but that's not read by llvm
 export CFLAGS="${CPPFLAGS}"
@@ -39,43 +26,49 @@ rm -fr /pkg/main/${PKG}.data.${PVRF}/config
 
 CMAKE_OPTS=(
 	-DCMAKE_INSTALL_PREFIX="/pkg/main/${PKG}.data.${PVRF}" # use data to avoid addition to PATH
+	-DCMAKE_BUILD_TYPE=Release
 
-	-C "$S/../clang/cmake/caches/DistributionExample.cmake"
+	-DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;lld;lldb;mlir;openmp;polly;bolt"
+	-DLLVM_TARGETS_TO_BUILD=all
+	-DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind"
+	-DLLVM_ENABLE_ASSERTIONS=ON
+	-DLLVM_ENABLE_EH=ON
+	-DLLVM_ENABLE_RTTI=ON
+	-DLLVM_BUILD_EXAMPLES=OFF
+	-DLLVM_INCLUDE_TESTS=OFF
+	-DLLVM_BUILD_TESTS=OFF
+	-DLLVM_INCLUDE_DOCS=ON
+	-DLLVM_ENABLE_DOXYGEN=OFF
+	-DLLVM_ENABLE_SPHINX=ON
 
-	-DCMAKE_C_FLAGS="${CPPFLAGS} -O2"
-	-DCMAKE_CXX_FLAGS="${CPPFLAGS} -O2"
 	-DCMAKE_SYSTEM_INCLUDE_PATH="${CMAKE_SYSTEM_INCLUDE_PATH}"
 	-DCMAKE_SYSTEM_LIBRARY_PATH="${CMAKE_SYSTEM_LIBRARY_PATH}"
-	-DLLVM_INCLUDE_TESTS=OFF
 
 	-DZLIB_LIBRARY=/pkg/main/sys-libs.zlib.libs.${OS}.${ARCH}/lib$LIB_SUFFIX/libz.so
 	-DZLIB_INCLUDE_DIR=/pkg/main/sys-libs.zlib.dev.${OS}.${ARCH}/include
 
 	-DLLVM_HOST_TRIPLE="${CHOST}"
-
 	-DLLVM_LIBDIR_SUFFIX=$LIB_SUFFIX
 
 	-DDEFAULT_SYSROOT="/pkg/main/sys-libs.glibc.dev.${OS}.${ARCH}"
 	#-DC_INCLUDE_DIRS="/pkg/main/sys-libs.glibc.dev.${OS}.${ARCH}/include"
 
-	-DLIBCXXABI_USE_LLVM_UNWINDER=OFF
+	-DLIBCXXABI_USE_LLVM_UNWINDER=ON
 
 	-DPython3_EXECUTABLE=/bin/python3
+	-DLLDB_PYTHON_EXE_RELATIVE_PATH=../dev-lang.python.core/bin/
 
 	# force llvm defaults
 	-DCLANG_DEFAULT_CXX_STDLIB="libc++"
-	#-DCLANG_DEFAULT_RTLIB="compiler-rt"
-	#-DCLANG_DEFAULT_UNWINDLIB="libunwind"
+	-DCLANG_DEFAULT_RTLIB="compiler-rt"
+	-DCLANG_DEFAULT_UNWINDLIB="libunwind"
 	-DCLANG_CONFIG_FILE_SYSTEM_DIR="/pkg/main/${PKG}.data.${PVRF}/config"
-
-	# ensure DEFAULT_SYSROOT is passed to the subsequent clang
-	-DCLANG_BOOTSTRAP_PASSTHROUGH="DEFAULT_SYSROOT;CMAKE_SYSTEM_INCLUDE_PATH;CMAKE_SYSTEM_LIBRARY_PATH;LLVM_HOST_TRIPLE;LLVM_LIBDIR_SUFFIX;ZLIB_LIBRARY;ZLIB_INCLUDE_DIR;LIBCXXABI_USE_LLVM_UNWINDER;CLANG_DEFAULT_CXX_STDLIB;CLANG_CONFIG_FILE_SYSTEM_DIR"
 )
 
 # do not use llvmbuild since we are building llvm itself
 # do not use docmake either since we want this to be contained in a data dir
 cmake -S "${S}" -B "${T}" -G Ninja -Wno-dev "${CMAKE_OPTS[@]}"
-ninja -j"$NPROC" -v stage2
+ninja -j"$NPROC" -v all
 
 if [ x"$LIB_SUFFIX" != x ]; then
 	# pre-create a symlink for lib → lib$LIB_SUFFIX
@@ -83,7 +76,7 @@ if [ x"$LIB_SUFFIX" != x ]; then
 	ln -snf "lib$LIB_SUFFIX" "${D}/pkg/main/${PKG}.data.${PVRF}/lib"
 fi
 
-DESTDIR="${D}" ninja -j"$NPROC" -v stage2-install
+DESTDIR="${D}" ninja -j"$NPROC" -v install
 
 mkdir -p "${D}/pkg/main/${PKG}.data.${PVRF}/config"
 echo "@clang-common.cfg" >"${D}/pkg/main/${PKG}.data.${PVRF}/config/clang.cfg"
@@ -93,14 +86,10 @@ echo "@clang-common.cfg" >"${D}/pkg/main/${PKG}.data.${PVRF}/config/clang-cpp.cf
 echo "@clang-cxx.cfg" >>"${D}/pkg/main/${PKG}.data.${PVRF}/config/clang-cpp.cfg"
 
 cat >"${D}/pkg/main/${PKG}.data.${PVRF}/config/clang-common.cfg" <<EOF
-# --rtlib=compiler-rt
---unwindlib=libgcc
--fuse-ld=bfd
+-fuse-ld=lld
 EOF
 
 cat >"${D}/pkg/main/${PKG}.data.${PVRF}/config/clang-cxx.cfg" <<EOF
---stdlib=libc++
-
 # fix clang include path order
 -nostdinc
 -isystem /pkg/main/${PKG}.data.${PVRF}/include/c++/v1
