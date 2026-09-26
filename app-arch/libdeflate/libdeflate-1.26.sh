350efca823fd32fbcd178174521d85d55ae008fb
kokoye2007 <kokoye2007@gmail.com>
2026-09-27 04:14:31 +1000

    Update app-arch/libdeflate to 1.26

---
+++ app-arch/libdeflate/libdeflate-1.26.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+get https://github.com/ebiggers/libdeflate/archive/refs/tags/v${PV}.tar.gz ${P}.tar.gz
+acheck
+
+cd "${P}"
+
+MAKEOPTS=(
+	USE_SHARED_LIB=1
+	PREFIX="/pkg/main/${PKG}.core.${PVRF}"
+	INCDIR="/pkg/main/${PKG}.dev.${PVRF}/include"
+	LIBDIR="/pkg/main/${PKG}.libs.${PVRF}/lib$LIB_SUFFIX"
+	DESTDIR="${D}"
+)
+
+make "${MAKEOPTS[@]}"
+make "${MAKEOPTS[@]}" install
+
+finalize
